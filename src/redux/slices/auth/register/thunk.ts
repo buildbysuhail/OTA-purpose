31f5c518eb84c169f9e39dd53c448614cd78c6ad
safvanpp <safvan.work@gmail.com>
2024-09-04 12:17:31 +0530

    for vercel Test

--- src/redux/slices/auth/register/thunk.ts
+++ src/redux/slices/auth/register/thunk.ts
@@ -1,10 +1,4 @@
 //Include Both Helper File with needed methods
-import { getFirebaseBackend } from "../../../helpers/firebase_helper";
-import {
-  postFakeRegister,
-  postJwtRegister,
-} from "../../../helpers/fakebackend_helper";
-
 // action
 import {
   registerUserSuccessful,
@@ -14,29 +8,29 @@ import {
 } from "./reducer";
 
 // initialize relavant method of both Auth
-const fireBaseBackend: any = getFirebaseBackend();
+// const fireBaseBackend: any = getFirebaseBackend();
 
 // Is user register successfull then direct plot user in redux.
 export const registerUser = (user: any) => async (dispatch: any) => {
   try {
     let response;
 
-    if (process.env.REACT_APP_DEFAULTAUTH === "firebase") {
-      response = fireBaseBackend.registerUser(user.email, user.password);
-      // yield put(registerUserSuccessful(response));
-    } else if (process.env.REACT_APP_DEFAULTAUTH === "jwt") {
-      response = postJwtRegister('/post-jwt-register', user);
-      // yield put(registerUserSuccessful(response));
-    } else if (process.env.REACT_APP_API_URL) {
-      response = postFakeRegister(user);
-      const data: any = await response;
+    // if (process.env.REACT_APP_DEFAULTAUTH === "firebase") {
+    //   response = fireBaseBackend.registerUser(user.email, user.password);
+    //   // yield put(registerUserSuccessful(response));
+    // } else if (process.env.REACT_APP_DEFAULTAUTH === "jwt") {
+    //   response = postJwtRegister('/post-jwt-register', user);
+    //   // yield put(registerUserSuccessful(response));
+    // } else if (process.env.REACT_APP_API_URL) {
+    //   response = postFakeRegister(user);
+    //   const data: any = await response;
 
-      if (data.message === "success") {
-        dispatch(registerUserSuccessful(data));
-      } else {
-        dispatch(registerUserFailed(data));
-      }
-    }
+    //   if (data.message === "success") {
+    //     dispatch(registerUserSuccessful(data));
+    //   } else {
+    //     dispatch(registerUserFailed(data));
+    //   }
+    // }
   } catch (error) {
     dispatch(registerUserFailed(error));
   }

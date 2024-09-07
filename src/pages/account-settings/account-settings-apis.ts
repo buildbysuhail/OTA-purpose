74c9162e108f009d096a72def62a83e123a58760
Sreeram <msreeram713@gmail.com>
2024-09-07 19:30:59 +0530

    fix

--- src/pages/account-settings/account-settings-apis.ts
+++ src/pages/account-settings/account-settings-apis.ts
@@ -6,6 +6,7 @@ const AccountSettingsApis = {
 
   getAvailableSessionsForDxGrid: async (loadOptions: any) => {
     try {
+      debugger;
       const responseData = await api.get(Urls.getUserSession, loadOptions);
       return responseData;
     } catch (error) {

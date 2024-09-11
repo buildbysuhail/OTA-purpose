311ea2871bee243ad4311d1ae04c050c7f1daa54
nizam-bit <nizamkarippali007@gmail.com>
2024-09-11 18:53:34 +0530

    commit sycnk

--- src/pages/account-settings/account-settings-sessions.tsx
+++ src/pages/account-settings/account-settings-sessions.tsx
@@ -18,7 +18,8 @@ import {
   Paging,
   Scrolling,
   SearchPanel,
-  DataGridTypes
+  DataGridTypes,
+  ColumnFixing
 } from "devextreme-react/cjs/data-grid";
 import CustomStore from "devextreme/data/custom_store";
 import AccountSettingsApis from "./account-settings-apis";
@@ -27,6 +28,8 @@ import chrome from '../../assets/images/browser-logos/chrome.png';
 import firefox from '../../assets/images/browser-logos/firefox.png';
 import microsoft from '../../assets/images/browser-logos/microsoft.png';
 import safari from '../../assets/images/browser-logos/safari.png';
+// import { deviceLogos } from "../../assets/images/device-logos";
+
 interface AccountSettingsProps {}
 
 const AccountSettingsSessions: FC<AccountSettingsProps> = (props) => {
@@ -130,26 +133,68 @@ const AccountSettingsSessions: FC<AccountSettingsProps> = (props) => {
   
     return (
       <div className="flex justify-start items-center gap-1">
-        {browserImage && <img src={browserImage} alt={data.data.browser} style={{ width: '20px', height: '20px' }} />}
+        {browserImage && <img src={browserImage} alt={data.data.browser} style={{ width: '18px', height: '18px' }} />}
         <span>{data.data.browser}</span>
+        <i className="ri-checkbox-blank-circle-fill drop-shadow-md" style={{ color: data.data.isActive ? '#22c55e' : '#57534e',fontSize: '7px' }}></i>
       </div>
     );
   };
 
  const renderCountryCell = (data: DataGridTypes.ColumnCellTemplateData)=>(
-  <div className="flex justify-start items-center gap-0.5">
+  <div className="flex justify-start items-center gap-1">
    <img
-      src={data.data.country_flag ? data.data.country_flag : "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBA=="}
+      src={data.data.country_flag ? data.data.country_flag : ""}
       alt={``}
-      className="object-scale-down"
-      style={{ width: '40px', height: '20px', minWidth: '40px', minHeight: '20px' }} 
+      className="aspect-square  rounded-full"
+      style={{ width: '18px', height: '18px',}} 
     />
-    <span>{`${data.data.state}-${data.data.country}`}</span>
+    <span>{`${data.data.country},${data.data.state}`}</span>
 </div>
  )
 
-//  const renderDeviceCell = (data: DataGridTypes.ColumnCellTemplateData)=>{}
+ const renderDeviceCell = (data: DataGridTypes.ColumnCellTemplateData)=>{
+  // let deviceImage = '';
+  let iconclass = "";
+  // <i className="ri-windows-fill"></i>
+  switch (data.data.device) {
+    case 'Windows':
+    //  deviceImage = deviceLogos.windows;
+    iconclass = "ri-windows-fill"
+      break;
+    case 'Linux':
+    //  deviceImage = deviceLogos.linux;
+  
+     iconclass = "ri-ubuntu-fill"
+      break;
+    case 'Android':
+    //  deviceImage = deviceLogos.android;
+      break;
+    case 'Mac':
+    //  deviceImage = deviceLogos.mac;
+      break;
+      case 'Ios':
+    //  deviceImage = deviceLogos.ios;
+      break; 
+    default:
+    //  deviceImage = ''; // You can add a default image or leave it empty
+  };
 
+  return (
+    <div className="flex justify-start items-center gap-1">
+      {/* {deviceImage && <img src={deviceImage} alt={data.data.device} className="aspect-square object-contain" style={{ width: '17px', height: '17px' }} />} */}
+      {iconclass  && <i className={`${iconclass}  text-[20px] text-sky-600`} ></i>}
+      <span className="">{data.data.device} </span>
+      
+    </div>
+  );
+ }
+
+
+ const renderCellHeader = (data:any) => {
+ return  <span className="font-semibold text-black font-sans text-[13px] py-2">
+  {data.column.caption}
+</span>
+ }
  
 //  ==========================================================================================
   return (
@@ -170,29 +215,39 @@ const AccountSettingsSessions: FC<AccountSettingsProps> = (props) => {
                   <p className="box-title-desc mb-0 text-[#8c9097] dark:text-white/50 font-weight:300 text-[0.75rem] opacity-[0.7]">
                   View and manage devices where you're currently logged in
                   </p>
+                  
                 </div>
                 <div></div>
               </div>
               <div className="box-body">
                 <div className="grid grid-cols-1 gap-3">
+
                   <DataGrid
                     height={gridHeight}
                     dataSource={
                       store
                       // "https://localhost:7213/api/Core/LoginSessions/GetAllAsync"
                     }
+                  
                     showBorders={true}
-                    columnAutoWidth={true}
+                    // columnAutoWidth={true}
                     // remoteOperations={true}
                     showColumnLines={false}
                     showRowLines={true}
-                    onRowPrepared={(e: any) => {
-                      if (e.rowType === "data" && e.data.isActive) {
-                        e.rowElement.style.backgroundColor = "#90ee90"; // Apply green background for active rows
-                      }
-                    }}
+                   
+                    // onRowPrepared={(e: any) => {
+                    //   if (e.rowType === "data" && e.data.isActive) {
+                    //     e.rowElement.style.backgroundColor = "#90ee90"; // Apply green background for active rows
+                    //   }
+                    // }}
                   >
-                    {/* <Scrolling mode="virtual" rowRenderingMode="virtual" /> */}
+                    <ColumnFixing enabled={true}/>
+                    {/* <Scrolling  mode="standard" 
+                      scrollByContent={true}
+                      useNative={false}
+                      scrollByThumb={true}
+                      showScrollbar="always"
+                    rowRenderingMode="virtual" /> */}
                     <Paging defaultPageSize={100} />
                     {/* <FilterRow visible={true} applyFilter="auto" />
       <HeaderFilter visible={true} />
@@ -203,30 +258,39 @@ const AccountSettingsSessions: FC<AccountSettingsProps> = (props) => {
                       allowSearch={true}
                       allowFiltering={true}
                       dataField="branchName"
-                      caption={"BranchName"}
+                      headerCellRender={renderCellHeader}
+                      caption="  Branch Name"
                       dataType="string"
                     />
                     <Column
+                      // width={110}
                       allowSearch={true}
                       allowFiltering={true}
                       dataField="browser"
                       cellRender={renderBrowserCell}
-                      caption={"browser"}
+                      calculateSortValue={(rowData) => (
+                       rowData.isActive ? 0 : 1 
+                      )}
+                      sortOrder="asc"
+                      caption={"Browser"}
+                      headerCellRender={renderCellHeader}
                       dataType="string"
                     />
                     <Column
                       allowSearch={true}
                       allowFiltering={true}
                       dataField="ipAddress"
-                      caption={"ipAddress"}
+                      caption={"IP Address"}
+                      headerCellRender={renderCellHeader}
                       dataType="string"
                     />
                     <Column
                       allowSearch={true}
                       allowFiltering={true}
                       dataField="device"
-                      // cellRender={renderDeviceCell}
-                      caption={"device"}
+                      cellRender={renderDeviceCell}
+                      caption={"Device"}
+                      headerCellRender={renderCellHeader}
                       dataType="string"
                     />
                     <Column
@@ -234,30 +298,47 @@ const AccountSettingsSessions: FC<AccountSettingsProps> = (props) => {
                       allowFiltering={true}
                       cellRender={renderCountryCell}
                       dataField="location"
-                      caption={"location"}
+                      caption={"Location"}
+                      headerCellRender={renderCellHeader}
                       dataType="string"
                     />
-                    <Column
+                    {/* <Column
                       allowSearch={true}
                       allowFiltering={true}
                       dataField="latitude"
                       caption={"latitude"}
                       dataType="string"
-                    />
-                    <Column
+                    /> */}
+                    {/* <Column
                       allowSearch={true}
                       allowFiltering={true}
                       dataField="longitude"
                       caption={"Longitude"}
                       dataType="string"
-                    />
+                    /> */}
                     <Column
                       allowSearch={true}
                       allowFiltering={true}
                       dataField="recentActivity"
-                      caption={"RecentActivity"}
+                      caption={"Recent Activity"}
+                      headerCellRender={renderCellHeader}
                       dataType="datetime"
                     />
+                     <Column
+                       
+                      fixed={true} fixedPosition="right"
+                      cellRender={({ data }) => (
+                   
+                      // onClick={() => handleLogout(data)} 
+                     
+                      // <i className="ri-logout-box-r-fill text-sky-800  text-[23px] ml-2"></i>
+                     <i className="ri-logout-box-r-line text-sky-800 text-center  text-[20px]  pl-1" ></i>
+                     
+                       )}
+                    caption="" 
+                     width={50} 
+                    
+                     />
                     {/* <Column allowSearch={true} allowFiltering={true} dataField="IsActive" caption={'isActive'} dataType="boolean" /> */}
                     {/* <Column dataField="isDefault" caption={'Is Default'} cellRender={({ data }) => (
                 data.isDefault === true ? 

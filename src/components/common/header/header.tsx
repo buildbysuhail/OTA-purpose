44da6844b84ffe1ce482f5cfacad0ccc82cf4c2e
safvanpp <safvan.work@gmail.com>
2024-09-14 16:55:10 +0530

    For Latest

--- src/components/common/header/header.tsx
+++ src/components/common/header/header.tsx
@@ -754,7 +754,7 @@ let userSession  = useAppSelector((state: RootState) => state.UserSession);
                       </Link>
                     </li>
                     <li>
-                      <Link className="w-full ti-dropdown-item !text-[0.8125rem] !gap-x-0  !p-[0.65rem] !inline-flex" to="#"><i
+                      <Link className="w-full ti-dropdown-item !text-[0.8125rem] !gap-x-0  !p-[0.65rem] !inline-flex" to="/switch-organization"><i
                         className="ti ti-inbox text-[1.125rem] me-2 opacity-[0.7]"></i>Organizations <span
                           className="!py-1 !px-[0.45rem] !font-semibold !rounded-sm text-success text-[0.75em] bg-success/10 ms-auto">25</span>
                       </Link>

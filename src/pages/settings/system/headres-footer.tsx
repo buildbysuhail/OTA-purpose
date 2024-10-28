3e7d70cd3ebdbadb9ba3778fa9605ef0fd456100
vajidqrasys <vajidqrasys@gmail.com>
2024-10-28 16:41:04 +0530

    headers and footers translated

--- src/pages/settings/system/headres-footer.tsx
+++ src/pages/settings/system/headres-footer.tsx
@@ -62,151 +62,151 @@ const HeadersAndFooters : React.FC = React.memo(() => {
       <div className="w-full pt-4">
     <div className='flex flex-col sm:flex-row justify-start items-center gap-4'>
       <div className='flex flex-col gap-4'>
-        <h4 className='font-medium text-gray-500'>Headers</h4>
+        <h4 className='font-medium text-gray-500'>{t("headers")}</h4>
         <div className="grid grid-cols-1 sm:grid-cols-2  gap-3">
         <ERPInput
           {...getFieldProps("heading1")}
-          label="Header 1"
-          placeholder="heading1"
+          label={t("header_1")}
+          placeholder={t("heading_1")}
           required={false}
           onChangeData={(data: any) => handleFieldChange("heading1", data.heading1)}
           
         />
           <ERPInput
           {...getFieldProps("heading2")}
-          label="Header 2"
-          placeholder="heading2"
+          label={t("header_2")}
+          placeholder={t("heading_2")}
           required={false}
           onChangeData={(data: any) => handleFieldChange("heading2", data.heading2)}
         />
           <ERPInput
           {...getFieldProps("heading3")}
-          label="Header 3"
-          placeholder="heading3"
+          label={t("header_3")}
+          placeholder={t("heading_3")}
           required={false}
           onChangeData={(data: any) => handleFieldChange("heading3", data.heading3)}
         />
           <ERPInput
           {...getFieldProps("heading4")}
-          label="Header 4"
-          placeholder="heading4"
+          label={t("header_4")}
+          placeholder={t("heading_4")}
           required={false}
           onChangeData={(data: any) => handleFieldChange("heading4", data.heading4)}
         />
           <ERPInput
           {...getFieldProps("heading5")}
-          label="Header 5"
-          placeholder="heading5"
+          label={t("header_5")}
+          placeholder={t("heading_5")}
           required={false}
           onChangeData={(data: any) => handleFieldChange("heading5", data.heading5)}
         />
           <ERPInput
           {...getFieldProps("heading6")}
-          label="Header 6"
-          placeholder="heading6"
+          label={t("header_6")}
+          placeholder={t("heading_6")}
           required={false}
           onChangeData={(data: any) => handleFieldChange("heading6", data.heading6)}
         />
           <ERPInput
           {...getFieldProps("heading7")}
-          label="Header 7/Report1"
-          placeholder="heading7"
+          label={t("header_7/report_1")}
+          placeholder={t("heading_7")}
           required={false}
           onChangeData={(data: any) => handleFieldChange("heading7", data.heading7)}
         />
           <ERPInput
           {...getFieldProps("heading8")}
-          label="Header 8/Report2"
-          placeholder="header 8 "
+          label={t("header_8/report_2")}
+          placeholder={t("header_8")}
           required={false}
           onChangeData={(data: any) => handleFieldChange("heading8", data.heading8)}
         />
           <ERPInput
           {...getFieldProps("heading9")}
-          label="Header  9/Report3"
-          placeholder="heading9"
+          label={t("header_9/report_3")}
+          placeholder={t("heading_9")}
           required={false}
           onChangeData={(data: any) => handleFieldChange("heading9", data.heading9)}
         />
           <ERPInput
           {...getFieldProps("heading10")}
-          label="Header 10/Barcode Title"
-          placeholder="heading10"
+          label={t("header_10/barcode_title")}
+          placeholder={t("heading_10")}
           required={false}
           onChangeData={(data: any) => handleFieldChange("heading10", data.heading10)}
         />
         </div>
     </div>
         <div className='flex flex-col gap-4'>
-        <h4 className='font-medium text-gray-500'>Footer</h4>
+        <h4 className='font-medium text-gray-500'>{t("footer")}</h4>
         <div className="grid grid-cols-1 sm:grid-cols-2  gap-3">
         <ERPInput
           {...getFieldProps("footer1")}
-          label="Footer 1"
-          placeholder="footer1"
+          label={t("footer_1")}
+          placeholder={t("footer_1")}
           required={false}
           onChangeData={(data: any) => handleFieldChange("footer1", data.footer1)}
         />
          <ERPInput
           {...getFieldProps("footer2")}
-          label="Footer 2"
-          placeholder="footer2"
+          label={t("footer_2")}
+          placeholder={t("footer_2")}
           required={false}
           onChangeData={(data: any) => handleFieldChange("footer2", data.footer2)}
         />
          <ERPInput
           {...getFieldProps("footer3")}
-          label="Footer 3"
-          placeholder="footer3"
+          label={t("footer_3")}
+          placeholder={t("footer_3")}
           required={false}
           onChangeData={(data: any) => handleFieldChange("footer3", data.footer3)}
         />
          <ERPInput
           {...getFieldProps("footer4")}
-          label="Footer 4"
-          placeholder="footer4"
+          label={t("footer_4")}
+          placeholder={t("footer_4")}
           required={false}
           onChangeData={(data: any) => handleFieldChange("footer4", data.footer4)}
         />
          <ERPInput
           {...getFieldProps("footer5")}
-          label="Footer 5"
-          placeholder="footer5"
+          label={t("footer_5")}
+          placeholder={t("footer_5")}
           required={false}
           onChangeData={(data: any) => handleFieldChange("footer5", data.footer5)}
         />
          <ERPInput
           {...getFieldProps("footer6")}
-          label="Footer 6"
-          placeholder="footer6"
+          label={t("footer_6")}
+          placeholder={t("footer_6")}
           required={false}
           onChangeData={(data: any) => handleFieldChange("footer6", data.footer6)}
         />
          <ERPInput
           {...getFieldProps("footer7")}
-          label="Footer 7"
-          placeholder="footer7"
+          label={t("footer_7")}
+          placeholder={t("footer_7")}
           required={false}
           onChangeData={(data: any) => handleFieldChange("footer7", data.footer7)}
         />
          <ERPInput
           {...getFieldProps("footer8")}
-          label="Footer 8"
-          placeholder="footer8"
+          label={t("footer_8")}
+          placeholder={t("footer_8")}
           required={false}
           onChangeData={(data: any) => handleFieldChange("footer8", data.footer8)}
         />
          <ERPInput
           {...getFieldProps("footer9")}
-          label="Footer 9"
-          placeholder="footer9"
+          label={t("footer_9")}
+          placeholder={t("footer_9")}
           required={false}
           onChangeData={(data: any) => handleFieldChange("footer9", data.footer9)}
         />
          <ERPInput
           {...getFieldProps("footer10")}
-          label="Footer 10"
-          placeholder="footer10"
+          label={t("footer_10")}
+          placeholder={t("footer_10")}
           required={false}
           onChangeData={(data: any) => handleFieldChange("footer10", data.footer10)}
         />
@@ -215,7 +215,7 @@ const HeadersAndFooters : React.FC = React.memo(() => {
     </div> 
         <ERPFormButtons
           isEdit={isEdit}
-          title="Save"
+          title={t("save")}
           isLoading={isLoading}
           onSubmit={handleSubmit}
           onCancel={onClose}

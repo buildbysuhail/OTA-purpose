abf3081f533b246339ae88838e7d8c5992cc47bd
mahirpolosys <mahirpolosys@gmail.com>
2024-10-29 09:37:34 +0530

    ERPDevGrid all add className="p-4"

--- src/pages/accounts/masters/reports/account-receivable-aging-report.tsx
+++ src/pages/accounts/masters/reports/account-receivable-aging-report.tsx
@@ -115,8 +115,8 @@ const AccountReceivableAgingReport = () => {
     <Fragment>
       <div className="grid grid-cols-12 gap-x-6">
         <div className="xxl:col-span-12 xl:col-span-12 col-span-12">
-          <div className="box custom-box">
-            <div className="box-body">
+          <div className="">
+            <div className="p-4">
               <div className="grid grid-cols-1 gap-3">
                 <ErpDevGrid
                   columns={columns}

8a870b9dd9520ac9033beceda0de05365ba76414
vajidqrasys <vajidqrasys@gmail.com>
2024-11-05 15:57:05 +0530

    application settings updated

--- src/pages/settings/system/application-settings-main.tsx
+++ src/pages/settings/system/application-settings-main.tsx
@@ -31,11 +31,23 @@ const ERPSettingsFormMain = () => {
     loadSettings();
   }, []);
 
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+
+    const formContainer = document.querySelector('.settings-form-container') as HTMLElement;
+    if (formContainer) {
+      formContainer.style.overflow = 'auto';
+    }
+
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, []);
+
   const loadSettings = async () => {
     setLoading(true);
     try {
       const response = await api.getAsync(`${Urls.application_settings}main`);
-
       console.log(settings);
       setSettingsPrev(response);
       setSettings(response);

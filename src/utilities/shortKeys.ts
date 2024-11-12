6d2c5bf20547b62a76035f499f58788c5c167882
vajidqrasys <vajidqrasys@gmail.com>
2024-11-12 10:25:58 +0530

    shortkeys updated

--- src/utilities/shortKeys.ts
+++ src/utilities/shortKeys.ts
@@ -1,23 +1,92 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+interface ShortcutConfig {
+  key: string;
+  description: string;
+  action: () => void;
+}
 
-const PopupShortKey = () => {
-    const dispatch = useDispatch();
+const POPUP_CLOSE_EVENT = 'popup-close-event';
 
-    useEffect(() => {
-        const handleKeyDown = (event: KeyboardEvent) => {
-            if (event.ctrlKey && event.key === 'b') {
-            }
-        };
+const shortKeys: ShortcutConfig[] = [
+  {
+    key: 'ctrl+shift+b',
+    description: 'Close all popups',
+    action: () => {
+      const event = new CustomEvent(POPUP_CLOSE_EVENT);
+      document.dispatchEvent(event);
+    }
+  },
+  {
+    key: 'shift+b',
+    description: 'Go to previous page',
+    action: () => {
+      window.history.back();
+    }
+  },
+  {
+    key: 'alt+f',
+    description: 'Go to next page',
+    action: () => {
+      window.history.forward();
+    }
+  },
+  {
+    key: 'ctrl+x',
+    description: 'Go to Home',
+    action: () => {
+      window.location.assign('/');
+    }
+  }
+];
 
-        window.addEventListener('keydown', handleKeyDown);
+const handleKeyPress = (event: KeyboardEvent) => {
+  if (
+    event.target instanceof HTMLInputElement ||
+    event.target instanceof HTMLTextAreaElement ||
+    event.target instanceof HTMLSelectElement
+  ) {
+    return;
+  }
 
-        return () => {
-            window.removeEventListener('keydown', handleKeyDown);
-        };
-    }, [dispatch]);
+  const shortcut = shortKeys.find((s) => {
+    const modifiers = s.key.split('+');
+    const matchedModifiers = modifiers.filter((modifier) => {
+      switch (modifier) {
+        case 'ctrl':
+          return event.ctrlKey;
+        case 'alt':
+          return event.altKey;
+        case 'shift':
+          return event.shiftKey;
+        default:
+          return event.key.toLowerCase() === modifier.toLowerCase();
+      }
+    });
+    return matchedModifiers.length === modifiers.length && event.key.toLowerCase() === modifiers[modifiers.length - 1].toLowerCase();
+  });
 
-    return null;
+  if (shortcut) {
+    event.preventDefault();
+    shortcut.action();
+  }
 };
 
-export default PopupShortKey;
+let isInitialized = false;
+
+export const initializeShortKeys = () => {
+  if (!isInitialized) {
+    document.addEventListener('keydown', handleKeyPress);
+    isInitialized = true;
+  }
+};
+
+export const cleanupShortKeys = () => {
+  if (isInitialized) {
+    document.removeEventListener('keydown', handleKeyPress);
+    isInitialized = false;
+  }
+};
+
+initializeShortKeys();
+
+export { POPUP_CLOSE_EVENT };
+export default shortKeys;

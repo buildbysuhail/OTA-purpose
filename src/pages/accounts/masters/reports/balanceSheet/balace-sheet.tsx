5b8d0b8a4b4fdc539aed96dc9af3a3a1b58d1b6b
mahirpolosys <mahirpolosys@gmail.com>
2024-11-20 10:37:38 +0530

    BalanceSheet last update

--- src/pages/accounts/masters/reports/balanceSheet/balace-sheet.tsx
+++ src/pages/accounts/masters/reports/balanceSheet/balace-sheet.tsx
@@ -16,61 +16,52 @@ import { Link } from "react-router-dom";
 
 const api = new APIClient();
 
-const BalanceSheetRow: React.FC<{ item: any, setIsOpenDetails: any }> = ({ item }) => {
-  const [isOpenDetails, setIsOpenDetails] = useState<boolean>(false);
+const BalanceSheetRow: React.FC<{
+  item: any;
+  setIsOpenDetails: ({isOpen: boolean, key: string}) => void;
+}> = ({ item, setIsOpenDetails }) => {
   const { t } = useTranslation();
 
-  const handleClick: MouseEventHandler<HTMLAnchorElement> = (event: any) => {
-   
-    console.log('sdsdsdsd');
-    
-    setIsOpenDetails(true);
+  const handleClick: MouseEventHandler<HTMLAnchorElement> = (event) => {
+    event.preventDefault();
+    setIsOpenDetails({isOpen: true, key: item.groupID});  
   };
+
   return (
     <tr>
-      {/* <td
-        className={`py-2`}
-        style={{
-          paddingLeft: item.groupID == 0 ? "0px" : "10px",
-          fontWeight: item.groupID == 0 ? "bold" : "normal",
-        }}
-      >
-        <Link to={item.link} className="text-[#3b82f6] hover:text-[#1d4ed8]">
-        {item.groupName}
-        </Link>
-      </td> */}
-
       <td
         className={`py-2 ${
-          item.groupID == 0 ? "text-[#03070f] " : "text-[#3b82f6]"
+          item.groupID == 0 ? "text-[#03070f]" : "text-[#3b82f6]"
         }`}
         style={{
           paddingLeft: item.groupID == 0 ? "0px" : "10px",
           fontWeight: item.groupID == 0 ? "bold" : "normal",
         }}
       >
-        <a
-          onClick={handleClick}
-          className="hover:text-[#1d4ed8]"
-        >
-          dsd
+        <a href="#" onClick={handleClick} className="hover:text-[#1d4ed8]">
           {item.groupName}
         </a>
       </td>
       {item.total !== undefined && (
         <td className="py-2 text-right">
-          <Link to={item.link} className="text-[#3b82f6] hover:text-[#1d4ed8]">
+          <a
+            href="#"
+            // onClick={handleClick}
+            className="text-[#3b82f6] hover:text-[#1d4ed8]"
+          >
             {item.total}
-          </Link>
+          </a>
         </td>
       )}
-
-      
     </tr>
   );
 };
+
 // Horizontal format component
-const HorizontalBalanceSheet: React.FC<{ data: any, setIsOpenDetails: any}> = ({ data, setIsOpenDetails }) => {
+const HorizontalBalanceSheet: React.FC<{
+  data: any;
+  setIsOpenDetails: any;
+}> = ({ data, setIsOpenDetails }) => {
   const assets = data?.filter((item: any) => item?.transType == "A");
 
   const liabilities = data?.filter((item: any) => item?.transType == "L");
@@ -88,7 +79,11 @@ const HorizontalBalanceSheet: React.FC<{ data: any, setIsOpenDetails: any}> = ({
           </thead>
           <tbody>
             {assets?.map((item: any, index: number) => (
-              <BalanceSheetRow key={`asset-${index}`} item={item} setIsOpenDetails={setIsOpenDetails}/>
+              <BalanceSheetRow
+                key={`asset-${index}`}
+                item={item}
+                setIsOpenDetails={setIsOpenDetails}
+              />
             ))}
           </tbody>
         </table>
@@ -104,7 +99,11 @@ const HorizontalBalanceSheet: React.FC<{ data: any, setIsOpenDetails: any}> = ({
           </thead>
           <tbody>
             {liabilities?.map((item: any, index: number) => (
-              <BalanceSheetRow key={`liability-${index}`} item={item} setIsOpenDetails={setIsOpenDetails}/>
+              <BalanceSheetRow
+                key={`liability-${index}`}
+                item={item}
+                setIsOpenDetails={setIsOpenDetails}
+              />
             ))}
           </tbody>
         </table>
@@ -119,7 +118,7 @@ const BalanceSheet = () => {
   const [filter, setFilter] = useState<any>(BalanceSheetFilterInitialState);
   const [filterShowCount, setFilterShowCount] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(false);
-  const [isOpenDetails, setIsOpenDetails] = useState<boolean>(false);
+  const [isOpenDetails, setIsOpenDetails] = useState<{isOpen: boolean; key: number}>(false);
   const { t } = useTranslation();
 
   useEffect(() => {
@@ -221,7 +220,10 @@ const BalanceSheet = () => {
         ) : (
           <>
             {filter.showVertical != true ? (
-              <HorizontalBalanceSheet data={data ?? []} setIsOpenDetails={setIsOpenDetails}/>
+              <HorizontalBalanceSheet
+                data={data ?? []}
+                setIsOpenDetails={setIsOpenDetails}
+              />
             ) : (
               <table className="w-full text-left border-collapse">
                 <thead>
@@ -232,7 +234,11 @@ const BalanceSheet = () => {
                 </thead>
                 <tbody>
                   {data?.map((item, index) => (
-                    <BalanceSheetRow key={index} item={item ?? []} setIsOpenDetails={setIsOpenDetails} />
+                    <BalanceSheetRow
+                      key={index}
+                      item={item ?? []}
+                      setIsOpenDetails={setIsOpenDetails}
+                    />
                   ))}
                 </tbody>
               </table>
@@ -255,7 +261,7 @@ const BalanceSheet = () => {
         content={
           <BalancesheetDetails
             postData={{
-              accGroupID:  10,
+              accGroupID: 10,
               asOnDate: filter.asOnDate,
             }}
           />

4cacd4682f6da7cd3f75f32df44c6f677fe35a47
Ajml.tk <ajmltk.official@gmail.com>
2024-11-26 18:45:30 +0530

    api-client

--- src/helpers/api-client.ts
+++ src/helpers/api-client.ts
@@ -36,7 +36,7 @@ const setAuthorization = () => {
   const token = Cookies.get("token");
   if (token) axios.defaults.headers.common["Authorization"] = "Bearer " + token;
 
-  axios.defaults.headers.post["X-Software-Date"] = new Date().getDate().toString();
+  axios.defaults.headers.post["X-Software-Date"] = new Date().toDateString();
   axios.defaults.headers.post["X-Client-Date"] = new Date().toISOString();
 };
 

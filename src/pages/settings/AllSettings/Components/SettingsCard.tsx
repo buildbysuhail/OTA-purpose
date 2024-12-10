326b97fe52f30e3780d15341c0198d0737653935
vajidqrasys <vajidqrasys@gmail.com>
2024-12-10 16:45:23 +0530

    dark mode

--- src/pages/settings/AllSettings/Components/SettingsCard.tsx
+++ src/pages/settings/AllSettings/Components/SettingsCard.tsx
@@ -75,7 +75,7 @@ const SettingsCard: React.FC<SettingsCardProps> = ({ data }) => {
     <div className="w-auto bg-gray-50 rounded-lg p-5 border flex flex-grow ">
       <div className="flex flex-col gap-5">
         <div className="flex gap-2 items-center">
-          <p className="text-sm font-medium">{t(data?.title)}</p>
+          <p className="text-sm font-medium text-black  ">{t(data?.title)}</p>
         </div>
         <div className={`grid grid-cols-${data?.columns ? data?.columns : 1}`}>
           {distributedItems.map((columnItems: any, idx: number) => (
@@ -83,12 +83,12 @@ const SettingsCard: React.FC<SettingsCardProps> = ({ data }) => {
               {columnItems?.filter((x: any) => x.visible == true)?.map((route: any, routeIdx: number) => (
                 <div key={`JPKNE84_${routeIdx}`}>
                   {route?.disabled ? (
-                    <p className="text-xs cursor  transition-all ease-in-out">
+                    <p className="text-xs cursor   transition-all ease-in-out">
                       {t(route?.title)}
                     </p>
                   ) : (
                     <p
-                      className="text-xs cursor-pointer hover:text-accent transition-all ease-in-out p-1 hover:bg-gray-400 hover:rounded-[5px]"
+                      className="text-xs cursor-pointer hover:text-accent transition-all ease-in-out p-1 hover:bg-gray-400 hover:rounded-[5px] text-black"
                       onClick={() => {
                         route?.path && route?.type === "link"
                           ? navigate(route?.path)

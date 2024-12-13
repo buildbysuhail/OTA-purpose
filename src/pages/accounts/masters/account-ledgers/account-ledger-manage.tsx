eb5d6167bb290fdcb68688ce4207f8bdd10ac7bb
SafvanQrasys <safvan.qrasys@gmail.com>
2024-12-13 21:13:29 +0530

    Chartof Accounts Issue Fixes

--- src/pages/accounts/masters/account-ledgers/account-ledger-manage.tsx
+++ src/pages/accounts/masters/account-ledgers/account-ledger-manage.tsx
@@ -5,7 +5,10 @@ import ERPInput from "../../../../components/ERPComponents/erp-input";
 import ERPDataCombobox from "../../../../components/ERPComponents/erp-data-combobox";
 import Urls from "../../../../redux/urls";
 import { useFormManager } from "../../../../utilities/hooks/useFormManagerOptions";
-import { AccountLedgerData, initialAccountLedger } from "./account-ledger-types";
+import {
+  AccountLedgerData,
+  initialAccountLedger,
+} from "./account-ledger-types";
 import { useRootState } from "../../../../utilities/hooks/useRootState";
 import ERPCheckbox from "../../../../components/ERPComponents/erp-checkbox";
 import { useTranslation } from "react-i18next";
@@ -25,31 +28,57 @@ export const AccountLedgerManage = () => {
     handleClose,
     getFieldProps,
     isLoading,
-    formState
+    formState,
   } = useFormManager<AccountLedgerData>({
     url: Urls.account_ledger,
-    onSuccess: useCallback(() => dispatch(toggleAccountLedgerPopup({ isOpen: false, key: null, reload: true })), [dispatch]),
-    onClose: useCallback(() => dispatch(toggleAccountLedgerPopup({ isOpen: false, key: null,reload: false })), [dispatch]),
+    onSuccess: useCallback(
+      () =>
+        dispatch(
+          toggleAccountLedgerPopup({ isOpen: false, key: null, reload: true })
+        ),
+      [dispatch]
+    ),
+    onClose: useCallback(
+      () =>
+        dispatch(
+          toggleAccountLedgerPopup({ isOpen: false, key: null, reload: false })
+        ),
+      [dispatch]
+    ),
     key: rootState.PopupData.accountLedger.key,
     useApiClient: true,
-    initialData: initialAccountLedger
+    initialData: initialAccountLedger,
   });
-    useEffect(() => {
-      if(rootState.PopupData.accountLedger.data != undefined && rootState.PopupData.accountLedger.data != null && rootState.PopupData.accountLedger.data.groupId != undefined && rootState.PopupData.accountLedger.data.groupId != null)
-      {
-        handleFieldChange("accGroupID", rootState.PopupData.accountLedger.data.groupId)
-      }
-    },[rootState.PopupData.accountLedger.data?.groupId])
+
   useEffect(() => {
-    if(rootState.PopupData.accountLedger.key === undefined || rootState.PopupData.accountLedger.key === null ) {
+    if (
+      rootState.PopupData.accountLedger.key === undefined ||
+      rootState.PopupData.accountLedger.key === null
+    ) {
       load();
     }
-     }, []);
+    
+  }, [rootState.PopupData.accountLedger.data?.groupId]);
   const load = async () => {
     try {
       const res = await api.getAsync(Urls.data_getNextLedgerCode);
       if (res) {
-        handleFieldChange("ledgerCode", res.toString());
+        handleFieldChange({ledgerCode: res.toString(),accGroupID: rootState.PopupData.accountLedger.data.groupId });
+      }
+      else
+      {
+        if (
+          rootState.PopupData.accountLedger.data != undefined &&
+          rootState.PopupData.accountLedger.data != null &&
+          rootState.PopupData.accountLedger.data.groupId != undefined &&
+          rootState.PopupData.accountLedger.data.groupId != null
+        ) {
+         
+          handleFieldChange(
+            "accGroupID",
+            rootState.PopupData.accountLedger.data.groupId
+          );
+        }
       }
     } catch (error) {
       console.error("Failed to fetch the next ledger code:", error);
@@ -60,34 +89,42 @@ export const AccountLedgerManage = () => {
 
   return (
     <div className="w-full pt-4">
-      {rootState.PopupData.accountLedger.data?.groupId}
       <div className="grid grid-cols-2 gap-3">
         <ERPInput
-          {...getFieldProps('ledgerCode')}
+          {...getFieldProps("ledgerCode")}
           label={t("code")}
           placeholder={t("enter_code")}
           required={true}
-          onChangeData={(data: any) => handleFieldChange('ledgerCode', data.ledgerCode)}
+          onChangeData={(data: any) =>
+            handleFieldChange("ledgerCode", data.ledgerCode)
+          }
         />
         <ERPInput
-          {...getFieldProps('ledgerName')}
+          {...getFieldProps("ledgerName")}
           label={t("name")}
           placeholder={t("enter_name")}
           required={true}
-          onChangeData={(data: any) => handleFieldChange('ledgerName', data.ledgerName)}
+          onChangeData={(data: any) =>
+            handleFieldChange("ledgerName", data.ledgerName)
+          }
         />
         <ERPInput
-          {...getFieldProps('aliasName')}
+          {...getFieldProps("aliasName")}
           label={t("alias_name")}
           placeholder={t("enter_alias_name")}
-          onChangeData={(data: any) => handleFieldChange('aliasName', data.aliasName)}
+          onChangeData={(data: any) =>
+            handleFieldChange("aliasName", data.aliasName)
+          }
         />
         <ERPInput
-          {...getFieldProps('arabicName')}
+          {...getFieldProps("arabicName")}
           label={t("name_in_arabic")}
           placeholder={t("enter_name_in_arabic")}
-          onChangeData={(data: any) => handleFieldChange('arabicName', data.arabicName)}
+          onChangeData={(data: any) =>
+            handleFieldChange("arabicName", data.arabicName)
+          }
         />
+
         <ERPDataCombobox
           {...getFieldProps("accGroupID")}
           field={{
@@ -97,7 +134,28 @@ export const AccountLedgerManage = () => {
             valueKey: "id",
             labelKey: "name",
           }}
-          disabled={(rootState.PopupData.accountLedger.data != undefined && rootState.PopupData.accountLedger.data != null && rootState.PopupData.accountLedger.data.groupId != undefined && rootState.PopupData.accountLedger.data.groupId != null)}
+          onChangeData={(data: any) => {
+            handleFieldChange("accGroupID", data.accGroupID);
+          }}
+          disabled={(formState?.data?.accGroupId != undefined && formState?.data?.accGroupId > 0 && formState?.data?.isEditable != true) || (rootState.PopupData.accountGroup.data != undefined && rootState.PopupData.accountGroup.data != null && rootState.PopupData.accountGroup.data.groupId != undefined && rootState.PopupData.accountGroup.data.groupId != null)}
+          label={t("group_under")}
+        />
+
+        <ERPDataCombobox
+          {...getFieldProps("accGroupID")}
+          field={{
+            id: "accGroupID",
+            required: true,
+            getListUrl: Urls.data_acc_groups,
+            valueKey: "id",
+            labelKey: "name",
+          }}
+          // disabled={
+          //   rootState.PopupData.accountLedger.data != undefined &&
+          //   rootState.PopupData.accountLedger.data != null &&
+          //   rootState.PopupData.accountLedger.data?.groupId != undefined &&
+          //   rootState.PopupData.accountLedger.data?.groupId != null
+          // }
           onChangeData={(data: any) => {
             handleFieldChange("accGroupID", data.accGroupID);
           }}
@@ -105,47 +163,52 @@ export const AccountLedgerManage = () => {
         />
         {/* <div className="w-full max-w-md mx-auto"> */}
         <div className="flex  space-x-3">
-          {formState?.data?.ledgerID == undefined || formState?.data?.ledgerID <= 0 &&
-            <>
-              <div className="basis-2/3">
-                <ERPInput
-                  {...getFieldProps('opBalance')}
-                  label={t("opening_balance")}
-                  type="number"
-                  onChangeData={(data: any) => handleFieldChange('opBalance', data.opBalance)}
-                />
-              </div>
-              <div className="basis-1/3 translate-y-[17px]">
-                <ERPDataCombobox
-
-                  {...getFieldProps("drCr")}
-                  field={{
-                    id: "drCr",
-                    valueKey: "value",
-                    labelKey: "label",
-                  }}
-                  onChangeData={(data: any) => handleFieldChange("drCr", data.drCr)}
-                  label=" "
-                  enableClearOption={false}
-                  options={[
-                    { value: 'Dr', label: t('Dr') },
-                    { value: 'Cr', label: t('Cr') },
-                  ]}
-                />
-              </div>
-            </>
-          }
+          {formState?.data?.ledgerID == undefined ||
+            (formState?.data?.ledgerID <= 0 && (
+              <>
+                <div className="basis-2/3">
+                  <ERPInput
+                    {...getFieldProps("opBalance")}
+                    label={t("opening_balance")}
+                    type="number"
+                    onChangeData={(data: any) =>
+                      handleFieldChange("opBalance", data.opBalance)
+                    }
+                  />
+                </div>
+                <div className="basis-1/3 translate-y-[17px]">
+                  <ERPDataCombobox
+                    {...getFieldProps("drCr")}
+                    field={{
+                      id: "drCr",
+                      valueKey: "value",
+                      labelKey: "label",
+                    }}
+                    onChangeData={(data: any) =>
+                      handleFieldChange("drCr", data.drCr)
+                    }
+                    label=" "
+                    enableClearOption={false}
+                    options={[
+                      { value: "Dr", label: t("Dr") },
+                      { value: "Cr", label: t("Cr") },
+                    ]}
+                  />
+                </div>
+              </>
+            ))}
           {/* </div> */}
         </div>
       </div>
       <div className="flex items-center gap-3 mt-2">
-
         <div className="w-full">
           <ERPInput
-            {...getFieldProps('remarks')}
+            {...getFieldProps("remarks")}
             label={t("remarks")}
             placeholder={t("enter_remarks")}
-            onChangeData={(data: any) => handleFieldChange('remarks', data.remarks)}
+            onChangeData={(data: any) =>
+              handleFieldChange("remarks", data.remarks)
+            }
           />
         </div>
       </div>
@@ -154,12 +217,16 @@ export const AccountLedgerManage = () => {
         <ERPCheckbox
           {...getFieldProps("isBillwiseApplicable")}
           label={t("bill_wise_applicable")}
-          onChangeData={(data: any) => handleFieldChange("isBillwiseApplicable", data.isBillwiseApplicable)}
+          onChangeData={(data: any) =>
+            handleFieldChange("isBillwiseApplicable", data.isBillwiseApplicable)
+          }
         />
         <ERPCheckbox
           {...getFieldProps("isActive")}
           label={t("active")}
-          onChangeData={(data: any) => handleFieldChange("isActive", data.isActive)}
+          onChangeData={(data: any) =>
+            handleFieldChange("isActive", data.isActive)
+          }
         />
         {/* <ERPCheckbox
           {...getFieldProps("isEditable")}
@@ -174,12 +241,19 @@ export const AccountLedgerManage = () => {
         <ERPCheckbox
           {...getFieldProps("isCostCentreApplicable")}
           label={t("cost_center_applicable")}
-          onChangeData={(data: any) => handleFieldChange("isCostCentreApplicable", data.isCostCentreApplicable)}
+          onChangeData={(data: any) =>
+            handleFieldChange(
+              "isCostCentreApplicable",
+              data.isCostCentreApplicable
+            )
+          }
         />
         <ERPCheckbox
           {...getFieldProps("isCommon")}
           label={t("is_common")}
-          onChangeData={(data: any) => handleFieldChange("isCommon", data.isCommon)}
+          onChangeData={(data: any) =>
+            handleFieldChange("isCommon", data.isCommon)
+          }
         />
       </div>
       <ERPFormButtons
@@ -189,6 +263,6 @@ export const AccountLedgerManage = () => {
         onCancel={handleClose}
         onSubmit={handleSubmit}
       />
-    </div >
+    </div>
   );
 };

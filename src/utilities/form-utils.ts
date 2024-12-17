b6ccb82ff76f27bb2ac255d584d697a5969ce67a
SafvanQrasys <safvan.qrasys@gmail.com>
2024-12-17 15:06:54 +0530

    Grid Preference Issue Fixes

--- src/utilities/form-utils.ts
+++ src/utilities/form-utils.ts
@@ -2,88 +2,110 @@ import { FormField } from "./form-types";
 import { lowercaseAndAddUnderscore } from "./Utils";
 
 const getNestedValue = (obj: any, path: string) => {
-  return path.split('.').reduce((acc, part) => acc && acc[part], obj);
+  return path.split(".").reduce((acc, part) => acc && acc[part], obj);
 };
 
-export const getFieldPropsGlobal = 
-  (fieldId: string, data: any): FormField => {
-    
-    const _value = getNestedValue(data?.data ? data?.data : data, fieldId);
-    const value =  _value == undefined || _value == null || _value == "" ? "" : _value == 0 ? '0' : _value|| "";
-    const validation = getNestedValue(data?.validations, fieldId);
-    const checked = getNestedValue(data?.data ? data?.data : data, fieldId) || false;
+export const getFieldPropsGlobal = (fieldId: string, data: any): FormField => {
+  debugger;
+  const _value = getNestedValue(data?.data ? data?.data : data, fieldId);
+  let value;
+  if (_value === undefined || _value === null || _value === "") {
+    value = "";
+  } else if (_value === 0) {
+    value = 0;
+  } else {
+    value = _value || "";
+  }
+  const validation = getNestedValue(data?.validations, fieldId);
+  const checked =
+    getNestedValue(data?.data ? data?.data : data, fieldId) || false;
 
-    return {
-      id: fieldId,
-      data: data?.data ? data?.data : data,
-      value,
-      validation,
-      checked,
-    };
-  };
-
-  const setNestedValueGlobal = (obj: any, path: string, value: any): any => {
-    const keys = path.split('.');
-    const lastKey = keys.pop() as string;
-    const deepClone = { ...obj }; // Create a shallow clone of the object
-    let nestedObj = deepClone;
-  
-    keys.forEach((key) => {
-      nestedObj[key] = nestedObj[key] ? { ...nestedObj[key] } : {};
-      nestedObj = nestedObj[key];
-    });
-  
-    nestedObj[lastKey] = value;
-  
-    return deepClone;
-  };
-  
-  // Reusable function to handle field changes
-  export const handleFieldChangeGlobal = ({
-    fields,
+  return {
+    id: fieldId,
+    data: data?.data ? data?.data : data,
     value,
-    formState,
-  }: {
-    fields: { [fieldId: string]: any } | string;
-    value?: any;
-    formState: any;
-  }) => {
-    // Convert single field updates to multi-field format
-    const fieldUpdates = typeof fields === 'string' ? { [fields]: value } : fields;
-  
-    // Update the nested fields for all provided fieldIds
-    return Object.entries(fieldUpdates).reduce(
-      (acc, [fieldId, val]) => setNestedValueGlobal(acc, fieldId, val),
-      formState?.data ? formState?.data: formState
-    );
+    validation,
+    checked,
   };
-  export const getFieldPropsAdvGlob = (
-    fieldId: string,
-    data: any,
-    handleFieldChange: (fieldId: string, value: any) => void,
-    t: any,
-    options?: { onChangeData?: (data: any) => void; label?: string }
-  ): FormField => {
-    
-    const __data = data?.data ? data?.data : data;
-    const _value = getNestedValue(__data, fieldId);
-    const value =  _value == undefined || _value == null || _value == "" ? "" : _value == 0 ? '0' : _value|| "";
-    const validation = getNestedValue(data?.validations, fieldId);
-    const checked = getNestedValue(data?.data ? data?.data : data, fieldId) || false;
-    const _label = options?.label || t( lowercaseAndAddUnderscore((fieldId?.split('.').pop()?.replace(/_/g, " ") || "")));
+};
 
-    return {
-      id: fieldId,
-      data: data?.data ? data?.data : data,
-      value,
-      validation,
-      checked,
-      onChangeData: options?.onChangeData
-        ? (event: any) => options.onChangeData?.(event.target.value)
-        : (eventData: any) => { const nestedValue = getNestedValue(eventData, fieldId);
+const setNestedValueGlobal = (obj: any, path: string, value: any): any => {
+  const keys = path.split(".");
+  const lastKey = keys.pop() as string;
+  const deepClone = { ...obj }; // Create a shallow clone of the object
+  let nestedObj = deepClone;
+
+  keys.forEach((key) => {
+    nestedObj[key] = nestedObj[key] ? { ...nestedObj[key] } : {};
+    nestedObj = nestedObj[key];
+  });
+
+  nestedObj[lastKey] = value;
+
+  return deepClone;
+};
+
+// Reusable function to handle field changes
+export const handleFieldChangeGlobal = ({
+  fields,
+  value,
+  formState,
+}: {
+  fields: { [fieldId: string]: any } | string;
+  value?: any;
+  formState: any;
+}) => {
+  // Convert single field updates to multi-field format
+  const fieldUpdates =
+    typeof fields === "string" ? { [fields]: value } : fields;
+
+  // Update the nested fields for all provided fieldIds
+  return Object.entries(fieldUpdates).reduce(
+    (acc, [fieldId, val]) => setNestedValueGlobal(acc, fieldId, val),
+    formState?.data ? formState?.data : formState
+  );
+};
+export const getFieldPropsAdvGlob = (
+  fieldId: string,
+  data: any,
+  handleFieldChange: (fieldId: string, value: any) => void,
+  t: any,
+  options?: { onChangeData?: (data: any) => void; label?: string }
+): FormField => {
+  const __data = data?.data ? data?.data : data;
+  const _value = getNestedValue(__data, fieldId);
+  const value =
+    _value == undefined || _value == null || _value == ""
+      ? ""
+      : _value == 0
+      ? "0"
+      : _value || "";
+  const validation = getNestedValue(data?.validations, fieldId);
+  const checked =
+    getNestedValue(data?.data ? data?.data : data, fieldId) || false;
+  const _label =
+    options?.label ||
+    t(
+      lowercaseAndAddUnderscore(
+        fieldId?.split(".").pop()?.replace(/_/g, " ") || ""
+      )
+    );
+
+  return {
+    id: fieldId,
+    data: data?.data ? data?.data : data,
+    value,
+    validation,
+    checked,
+    onChangeData: options?.onChangeData
+      ? (event: any) => options.onChangeData?.(event.target.value)
+      : (eventData: any) => {
+          const nestedValue = getNestedValue(eventData, fieldId);
           console.log("fieldId:", fieldId);
           console.log("eventData:", eventData);
-          console.log("nestedValue:", nestedValue);handleFieldChange(fieldId, nestedValue)},
-      label: _label,
-    };
-  }
+          console.log("nestedValue:", nestedValue);
+          handleFieldChange(fieldId, nestedValue);
+        },
+    label: _label,
+  };
+};

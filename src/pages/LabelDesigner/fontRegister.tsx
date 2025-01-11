4327c9ab5f3666f354580cc891b4f20d2104adda
nizam-bit <nizamkarippali007@gmail.com>
2025-01-11 17:12:06 +0530

    acc-transaction pdf view

--- src/pages/LabelDesigner/fontRegister.tsx
+++ src/pages/LabelDesigner/fontRegister.tsx
@@ -13,37 +13,54 @@ import FiraSansItalic from '../../assets/font/FiraSans/FiraSans-Italic.ttf'
 import RobotMonoRegular from '../../assets/font/RobotMono/RobotoMono-Regular.ttf'
 import RobotMonoBold from '../../assets/font/RobotMono/RobotoMono-Bold.ttf'
 import RobotMonoItalic from '../../assets/font/RobotMono/RobotoMono-Italic.ttf'
+
+// Import Poppins font files
+import PoppinsRegular from '../../assets/font/Poppins/Poppins-Regular.ttf';
+import PoppinsBold from '../../assets/font/Poppins/Poppins-Bold.ttf';
+import PoppinsItalic from '../../assets/font/Poppins/Poppins-Italic.ttf';
 // ../../assets/font/RobotMono/RobotMono-Italic.ttf
 export default function FontRegistration() {
-    useEffect(() => {
-      // Register fonts with explicit weight values
-      Font.register({
-        family: 'Roboto',
-        fonts: [
-          { src: RobotoRegular, fontWeight: 400, fontStyle: 'normal' },
-          { src: RobotoBold, fontWeight: 700, fontStyle: 'bold' },
-          { src: RobotoItalic, fontWeight: 400, fontStyle: 'italic' },
-        ],
-      })
-  
-      Font.register({
-        family: 'FiraSans',
-        fonts: [
-          { src: FiraSansRegular, fontWeight: 400, fontStyle: 'normal' },
-          { src: FiraSansBold, fontWeight: 700, fontStyle: 'bold' },
-          { src: FiraSansItalic, fontWeight: 400, fontStyle: 'italic' },
-        ],
-      })
-  
-      Font.register({
-        family: 'RobotoMono',
-        fonts: [
-          { src: RobotMonoRegular, fontWeight: 400, fontStyle: 'normal' },
-          { src: RobotMonoBold, fontWeight: 700, fontStyle: 'bold' },
-          { src: RobotMonoItalic, fontWeight: 400, fontStyle: 'italic' },
-        ],
-      })
-    }, [])
-  
-    return null
-  }
+  useEffect(() => {
+    // Register Roboto font
+    Font.register({
+      family: 'Roboto',
+      fonts: [
+        { src: RobotoRegular, fontWeight: 400, fontStyle: 'normal' },
+        { src: RobotoBold, fontWeight: 700, fontStyle: 'bold' },
+        { src: RobotoItalic, fontWeight: 400, fontStyle: 'italic' },
+      ],
+    });
+
+    // Register FiraSans font
+    Font.register({
+      family: 'FiraSans',
+      fonts: [
+        { src: FiraSansRegular, fontWeight: 400, fontStyle: 'normal' },
+        { src: FiraSansBold, fontWeight: 700, fontStyle: 'bold' },
+        { src: FiraSansItalic, fontWeight: 400, fontStyle: 'italic' },
+      ],
+    });
+
+    // Register RobotoMono font
+    Font.register({
+      family: 'RobotoMono',
+      fonts: [
+        { src: RobotMonoRegular, fontWeight: 400, fontStyle: 'normal' },
+        { src: RobotMonoBold, fontWeight: 700, fontStyle: 'bold' },
+        { src: RobotMonoItalic, fontWeight: 400, fontStyle: 'italic' },
+      ],
+    });
+
+    // Register Poppins font
+    Font.register({
+      family: 'Poppins',
+      fonts: [
+        { src: PoppinsRegular, fontWeight: 400, fontStyle: 'normal' },
+        { src: PoppinsBold, fontWeight: 700, fontStyle: 'bold' },
+        { src: PoppinsItalic, fontWeight: 400, fontStyle: 'italic' },
+      ],
+    });
+  }, []);
+
+  return null;
+}

2a1b9d224e2d189401d1f70ad0b68e1dff1e0734
nizam-bit <nizamkarippali007@gmail.com>
2025-01-14 21:35:37 +0530

    template balance sheet

--- src/pages/InvoiceDesigner/constants/TemplateCategories.ts
+++ src/pages/InvoiceDesigner/constants/TemplateCategories.ts
@@ -1,24 +1,4 @@
-// export type TemplateGroupTypes =
-//   | "sales_invoice"
-//   | "barcode"
-//   | "balance_sheet"
-//   | "cash_payment"
-//   | "cash_receipt"
-//   | "bank_payment"
-//   | "bank_receipt"
-//   | "cheque_payment"
-//   | "cheque_receipt"
-//   | "opening_balance"
-//   | "debit_note"
-//   | "credit_note"
-//   | "journal_entry"
-//   | "multi_journal_entry"
-//   | "closing_balance"
-//   | "pdc"
-//   | "bank_reconciliation"
-//   | "tax_on_expenses_payments"
-//   | "cash_payment_estimate"
-//   | "cash_receipt_estimate";
+
 
 import VoucherType from "../../../enums/voucher-types";
 

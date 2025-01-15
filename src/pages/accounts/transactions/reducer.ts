0761e9c84d096105932e8caaade057594d0cf00d
SafvanQrasys <safvan.qrasys@gmail.com>
2025-01-15 16:23:49 +0530

    AccTransactions modified

--- src/pages/accounts/transactions/reducer.ts
+++ src/pages/accounts/transactions/reducer.ts
@@ -426,9 +426,9 @@ const accTransactionSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder.addCase(loadAccVoucher.fulfilled, (state, action) => {
-      const applicationSettings =  useAppSelector((state: RootState) => state.ApplicationSettings);
+      // const applicationSettings =  useAppSelector((state: RootState) => state.ApplicationSettings);
       const payload = action.payload;
-  
+  debugger;
       if (payload) {
         state.row = { ...AccTransactionRowInitialData };
         // Handle master data

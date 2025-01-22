bc9b39073dae8fba05f258ea8f4014c1750512f8
mahirpolosys <mahirpolosys@gmail.com>
2025-01-22 12:02:31 +0530

    * dark mode added

--- src/pages/inventory/reports/stock-flow/stock-flow-report.tsx
+++ src/pages/inventory/reports/stock-flow/stock-flow-report.tsx
@@ -253,7 +253,7 @@ const StockFlow = () => {
   ];
   return (
     <Fragment>
-      <div className="grid grid-cols-12 gap-x-6 bg-[#fafafa]">
+      <div className="grid grid-cols-12 gap-x-6 dark:!bg-dark-bg bg-[#fafafa]">
         <div className="xxl:col-span-12 xl:col-span-12 col-span-12">
           <div className="">
             <div className="px-4 pt-4 pb-2 ">

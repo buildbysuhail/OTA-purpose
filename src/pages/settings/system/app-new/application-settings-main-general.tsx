0703e7878c8a6a483b537f4e13367276fac1c6d7
mahirpolosys <mahirpolosys@gmail.com>
2025-01-28 17:34:53 +0530

    Automatically select the first option

--- src/pages/settings/system/app-new/application-settings-main-general.tsx
+++ src/pages/settings/system/app-new/application-settings-main-general.tsx
@@ -112,6 +112,7 @@ const MainGeneralFilterableComponents: React.FC<ApplicationSettingsProps> = ({
           id="showNumberFormat"
           label={t("currency_format")}
           data={settings?.mainSettings}
+          value={-2}    
           onChangeData={(data) =>
             handleFieldChange(
               "mainSettings",

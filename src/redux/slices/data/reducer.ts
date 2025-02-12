35388002e0a58a49352722cc1e89f2f7e25b1a7d
nizam-bit <nizamkarippali007@gmail.com>
2025-02-12 18:16:06 +0530

    reducer.ts

--- src/redux/slices/data/reducer.ts
+++ src/redux/slices/data/reducer.ts
@@ -1,19 +1,24 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import moment from "moment";
 
-export interface ClientSessionModel {
-  ledgers?: [];
-
+export interface ledgerData {
+  ledgers?: any[];
 }
 // export const initialState : login  =  {loading: false, token: ""};
-export const initialState: ClientSessionModel = {
+export const initialState: ledgerData = {
 };
 const dataContainerSlice = createSlice({
   name: "dataContainer",
   initialState,
   reducers: {
-    setData: (state, action: PayloadAction<{key: keyof(ClientSessionModel), value: any}>) => {
-      state[key] = value
+    setData: (
+      state, 
+      action: PayloadAction<{key: keyof(ledgerData), value: any}>
+    ) => {
+      return {
+        ...state,
+        [action.payload.key]: action.payload.value
+      };
     },
 
   },

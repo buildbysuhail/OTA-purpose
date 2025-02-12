b372b5c6ac22424ded3b085d5df036650e7a8a46
nizam-bit <nizamkarippali007@gmail.com>
2025-02-12 17:36:50 +0530

    invocie_designer

---
+++ src/pages/InvoiceDesigner/DownloadPreview/advanced-payment/Header.tsx
@@ -0,0 +1,158 @@
+import { View, Text, Image,StyleSheet } from "@react-pdf/renderer";
+import { dateTrimmer, getAmountInWords } from "../../../../utilities/Utils";
+import useCurrentBranch from "../../../../utilities/hooks/use-current-branch";
+import { TemplateState } from "../../Designer/interfaces";
+
+const styles = StyleSheet.create({
+
+  companyInfo: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    width: "100%",
+    marginVertical:10,
+    zIndex: 10,
+  },
+  orgName: {
+    textTransform: "capitalize",
+    fontWeight: "semibold",
+  },
+  orgAddress: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent:"flex-start",
+    alignItems:"flex-end",
+    paddingRight:10,
+    gap:2,
+  },
+  otherInfo:{
+    display:"flex",
+    flexDirection:"row",
+    gap:2,
+    justifyContent:"flex-end"
+  },
+  bgImage: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    zIndex: -10,
+  },
+  docTitle:{
+    fontSize: 18,
+    fontWeight: "medium",
+    textDecoration:"underline"
+  },
+});
+
+export  const Header = ({ data, template, currentBranch,}: { data: any; template?: TemplateState; currentBranch: any,}) => {
+  const logoWidthRatio = template?.headerState?.logoSize ? template.headerState?.logoSize / 100 : 0.5;
+  const headerState = template?.headerState;
+
+
+  const fontFamily = template?.propertiesState?.font_family || "Roboto";
+  const fontSize = template?.propertiesState?.font_size || 12;
+  const color = template?.propertiesState?.font_color || "#000";
+  const fontWeight = template?.propertiesState?.font_weight || 400;
+  const fontStyle = template?.propertiesState?.fontStyle || "normal";
+
+  const orgNameFontColor = headerState?.OrganizationFontColor || "#000";
+  const orgNameFontSize = headerState?.OrganizationFontSize || 12;
+
+  const fontStyles = {
+    color,
+    fontSize,
+    fontWeight,
+    fontStyle,
+    fontFamily,
+  };
+  const labelStyles = {
+    color: template?.propertiesState?.label_font_color || "#000",
+    fontSize: template?.propertiesState?.label_font_size || 12,
+    fontWeight: template?.propertiesState?.label_font_weight || 400,
+    fontStyle:  template?.propertiesState?.label_font_style || "normal",
+    fontFamily,
+  };
+
+  const isValidLogo = (logo: any): boolean => {
+    if (!logo) return false;
+    if (typeof logo !== 'string') return false;
+    if (logo.trim() === '') return false;
+    // Add any other specific validation you need
+    return true;
+  };
+  
+  return (
+    <View style={{
+      width:"100%",
+      height:headerState?.headerHeight || undefined,
+      backgroundColor: template?.headerState?.bgColor || "#fff",
+      position: 'relative',
+      borderBottom:"1px solid rgb(104, 101, 101)",
+      zIndex:10
+      }}>
+  {/* Background Image */}
+    {template?.background_image_header && (
+        <Image
+          src={template?.background_image_header}
+          style={[
+            styles.bgImage,
+            { objectPosition: headerState?.bg_image_header_position || 'center' } // Control image position
+          ]}
+        />
+      )}
+      {/* Company Info */}
+      <View style={styles.companyInfo}>
+        <View style={{ display: "flex", flexDirection: "column",flexBasis:"33.33%",
+          justifyContent:"flex-start", alignItems:"flex-start",paddingLeft:10}}>
+             {headerState?.showLogo && isValidLogo(currentBranch?.logo) && (
+            <Image
+              src={currentBranch.logo}
+              style={{ width: 80 * logoWidthRatio }}
+            />
+             )}
+          {headerState?.showOrgName && (
+            <Text style={{ color: orgNameFontColor, fontSize: orgNameFontSize, fontWeight: "semibold",fontFamily:fontFamily,}}>
+              {currentBranch?.name}
+            </Text>
+          )}
+        </View>
+        <View style={{ flexBasis: "33.33%", display: "flex", justifyContent: "flex-end",alignItems:"center"  }}>
+         
+            <Text style={[styles.docTitle, { color: headerState?.docTitleFontColor, fontSize: headerState?.docTitleFontSize, fontFamily: fontFamily }]}>
+              {headerState?.docTitle || "Payment Voucher"}
+            </Text>
+          
+          </View>
+        <View style={[styles.orgAddress,{flexBasis:"33.33%",}]}>
+          {headerState?.showOrgAddress &&
+            currentBranch.address?.map((org: any, idx: number) => (
+              <Text key={`ADDK_${idx}`} style={fontStyles}>{org}</Text>
+            ))
+          }
+          {headerState?.hasPhoneField && (
+            <View style={styles.otherInfo}>
+              <Text style={labelStyles}>{headerState?.phoneLabel || "Phone No"}:</Text>
+              <Text style={fontStyles}>{currentBranch?.phone || "1234567891"}</Text>
+            </View>
+          )}
+          {headerState?.hasfaxField && (
+            <View style={styles.otherInfo}>
+              <Text style={labelStyles}>{headerState?.faxLabel || "Fax No"}:</Text>
+              <Text style={fontStyles}>{currentBranch?.fax || "##12344543"}</Text>
+            </View>
+          )}
+        </View>
+        
+      </View>
+    </View>
+  );
+};
+
+
+
+
+
+
+
---
+++ src/pages/InvoiceDesigner/DownloadPreview/advanced-payment/index.tsx
@@ -0,0 +1,51 @@
+import { Document, Page, View, Text, StyleSheet,PDFViewer,Image  } from "@react-pdf/renderer";
+import { TemplateState } from "../../Designer/interfaces";
+import FontRegistration from "../../../LabelDesigner/fontRegister";
+
+import { AccountTransactionProps } from "../account_transactiocn-premium";
+import { Header } from "./Header";
+import { Content } from "./Content";
+
+const AdvancedPayment = ({ data, template, currentBranch,userSession }: AccountTransactionProps) => {
+
+// Paddings
+const paddingLeft = template?.propertiesState?.padding?.left || 10;
+const paddingRight = template?.propertiesState?.padding?.right || 10;
+const paddingTop = template?.propertiesState?.padding?.top || 10;
+const paddingBottom = template?.propertiesState?.padding?.bottom || 10
+const pageOrientation = template?.propertiesState?.orientation === "landscape" ? "landscape" : "portrait";
+
+return (
+  <Document>
+  <FontRegistration />
+  <Page
+    size={"A4"}
+    orientation={pageOrientation}
+    wrap
+  >
+    {/* Main Container */}
+    <View style={{ width: '100%', height: '100%' ,
+     padding: `${paddingTop}pt ${paddingRight}pt ${paddingBottom}pt ${paddingLeft}pt`,
+   
+    }}>
+       
+      {/* Main Content */}
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        border:"1.5px solid rgb(104, 101, 101)"
+        
+      }}>
+          
+        {/* Header */}
+       <Header data={data} template={template} currentBranch={currentBranch} />
+        {/* Content */}
+          <Content data={data} template={template} currentBranch={currentBranch} />
+      </View>
+
+    </View>
+  </Page>
+</Document>
+);
+};
+export default AdvancedPayment;

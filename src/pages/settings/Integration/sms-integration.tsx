e373101ff4fe4d28f2f2683332522a2cb8ac1bf4
vajidqrasys <vajidqrasys@gmail.com>
2025-02-26 09:38:20 +0530

    sms integration updated

--- src/pages/settings/Integration/sms-integration.tsx
+++ src/pages/settings/Integration/sms-integration.tsx
@@ -18,22 +18,22 @@ import SMSTwilioConnectPopup from "./sms-twilio-connect-popup";
 const api = new APIClient();
 
 const SMSIntegration: React.FC = () => {
-  debugger;
   const [loading, setLoading] = useState(true);
   const [SubmittingSetAsDefault, setSubmittingSetAsDefault] = useState(false);
-  const [provider, setProvider] = useState<{isOpen: boolean, information?: information}>({isOpen: false, information:undefined});
+  const [provider, setProvider] = useState<{ isOpen: boolean, information?: information }>({
+    isOpen: false,
+    information: undefined,
+  });
   const [formState, setFormState] = useState<SMSIntegrationData[]>([]);
+  // state to store the selected integration
+  const [selectedIntegration, setSelectedIntegration] = useState<SMSIntegrationData | null>(null);
   const { t } = useTranslation("integration");
 
   useEffect(() => {
-    debugger;
-    console.log('SMSTwilioConnectPopup');
-    
     loadSettings();
   }, []);
 
   const loadSettings = async () => {
-    debugger;
     setLoading(true);
     const Channel = NotificationsChannel.Sms;
     try {
@@ -41,21 +41,20 @@ const SMSIntegration: React.FC = () => {
         `${Urls.notification_provider}?channel=${Channel}`
       );
       setFormState(response);
-     
     } catch (error) {
       console.error("Error loading settings:", error);
     } finally {
       setLoading(false);
     }
   };
+
   const setAsDefault = async () => {
     setSubmittingSetAsDefault(true);
     try {
       const requestBody = {
         provider: NotificationsProvider.TwillioSms,
-        channel: NotificationsChannel.Sms
+        channel: NotificationsChannel.Sms,
       };
-
       const response = await api.post(Urls.notification_provider_set_as_default, requestBody);
       await handleResponse(response);
     } catch (error) {
@@ -64,6 +63,7 @@ const SMSIntegration: React.FC = () => {
       setSubmittingSetAsDefault(false);
     }
   };
+
   const handleOpen = (configJson: any) => {
     let parsedConfig: any = {};
 
@@ -77,104 +77,98 @@ const SMSIntegration: React.FC = () => {
       parsedConfig = configJson;
     }
 
-    setProvider({isOpen: true, information: {
-      accountSid: parsedConfig.accountSid ?? "",
-      authToken: parsedConfig.authToken ?? "",
-      verifyServiceSid: parsedConfig.verifyServiceSid ?? "",
-      fromPhone: parsedConfig.fromPhone ?? "",
-    }});
-
+    setProvider({
+      isOpen: true,
+      information: {
+        accountSid: parsedConfig.accountSid ?? "",
+        authToken: parsedConfig.authToken ?? "",
+        verifyServiceSid: parsedConfig.verifyServiceSid ?? "",
+        fromPhone: parsedConfig.fromPhone ?? "",
+      },
+    });
   };
-  
+
   return (
-    <div className="p-6 max-w-8xl  mx-auto dark:bg-dark-bg bg-white dark:text-dark-text">
+    <div className="p-6 max-w-8xl mx-auto dark:bg-dark-bg bg-white dark:text-dark-text">
       <div className="xxl:h-[61.8rem]">
         <h1 className="text-2xl font-bold mb-4 dark:text-dark-text text-gray-800">
           {t("SMS_integrations")}
         </h1>
 
         {formState?.map((item) => (
-          <div
-            key={item.id}
-            className="flex items-center justify-between mb-4 p-4 dark:bg-dark-bg-header dark:text-dark-text bg-gray-50 rounded-lg"
-          >
-            <div className="flex items-center">
-              <div>
-                <h2 className="text-xl font-semibold dark:text-dark-text text-gray-700">
-                  {item.name}
-                </h2>
-                <p className="text-sm dark:text-dark-text text-gray-600">
-                  {item.description}
-                </p>
-              </div>
+          <div key={item.id} className="flex flex-col md:flex-row md:items-center justify-between mb-4 p-4 dark:bg-dark-bg-header dark:text-dark-text bg-gray-50 rounded-lg">
+            <div className="cursor-pointer" onClick={() => setSelectedIntegration(item)}>
+              <h2 className="text-xl font-semibold dark:text-dark-text text-gray-700">
+                {item.name}
+              </h2>
+              <p className="text-sm dark:text-dark-text text-gray-600">
+                {item.description}
+              </p>
             </div>
-            <div className="flex items-center gap-4">
+
+            <div className="mt-4 md:mt-0 flex items-center gap-4">
               <ERPButton
                 title={item.isEnable ? t("maintain") : t("connect")}
-                onClick={() => {debugger; handleOpen(item.configJson)}}
+                onClick={() => handleOpen(item.configJson)}
                 variant="primary"
-              ></ERPButton>
-              {/* <button onClick={() => setIsOpen(true)} className="rounded-sm px-4 py-2 bg-blue text-white rounded hover:bg-blue-600 transition-colors">
-            
-        </button> */}
+              />
               {item.isDefault ? (
-                <>
-                  <CircleCheck className="" />
-                </>
+                <CircleCheck className="" />
               ) : (
-                <ERPButton
-                  title={t("set as default")}
-                   onClick={setAsDefault}
-                ></ERPButton>
-                //   <button onClick={() => setIsOpen(true)} className="rounded-sm px-4 py-2 bg-blue text-white rounded hover:bg-blue-600 transition-colors">
-                //     {t("set as default")}
-                // </button>
+                <ERPButton title={t("set as default")} onClick={setAsDefault} />
               )}
             </div>
           </div>
-        ))}
-        <div className="mb-6">
-          <h3 className="text-lg font-semibold mb-2 dark:text-dark-text text-gray-700">
-            {t("benefits")}
-          </h3>
-          <ul className="list-disc pl-5 dark:text-dark-text text-gray-600">
-            <li className="pb-3">{t("notify_customers")}</li>
-            <li className="pb-3">{t("configure_SMS")}</li>
-          </ul>
-        </div>
 
-        <div>
-          <h3 className="text-lg font-semibold mb-2 dark:text-dark-text text-gray-700">
-            {t("before_you_can")}
-          </h3>
-          <ul className="list-disc pl-5 dark:text-dark-text text-gray-600">
-            <li className="pb-3">
-              {t("create_a_twilio_account")}{" "}
-              <a
-                href="https://www.twilio.com/try-twilio"
-                className="text-[#2589BD] hover:underline"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {t("sign_up_now")}
-              </a>
-            </li>
-            <li className="pb-3">{t("go_to_console")}</li>
-            <li className="pb-3">
-              {t("have_an_active_phone_number")}{" "}
-              <a
-                href="https://support.twilio.com/hc/en-us/articles/223135367-Phone-Number-types-Twilio-offers-and-how-they-work"
-                className="text-[#2589BD] hover:underline flex items-center"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {t("read_more")}
-                {/* <ExternalLink size={14} className="ml-1" /> */}
-                <i className="ri-external-link-line"></i>
-              </a>
-            </li>
-          </ul>
-          <SmsDemo />
+        ))}
+
+        {/* Conditional rendering of the lower section */}
+        <div className="mt-8">
+          {selectedIntegration ? (
+            // New content based on the selected integration
+            <div>
+              <h3 className="text-lg font-semibold mb-2 dark:text-dark-text text-gray-700">
+                {t("selected_integration")} : {selectedIntegration.name}
+              </h3>
+              <p className="mb-4 dark:text-dark-text text-gray-600">
+                {selectedIntegration.description}
+              </p>
+              {/* You can include additional details or components specific to the selected integration */}
+              <SmsDemo />
+            </div>
+          ) : (
+            // Default content
+            <div>
+              <h3 className="text-lg font-semibold mb-2 dark:text-dark-text text-gray-700">
+                {t("before_you_can")}
+              </h3>
+              <ul className="list-disc pl-5 dark:text-dark-text text-gray-600">
+                <li className="pb-3">
+                  {t("create_a_twilio_account")}{" "}
+                  <a
+                    href="https://www.twilio.com/try-twilio"
+                    className="text-[#2589BD] hover:underline"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    {t("sign_up_now")}
+                  </a>
+                </li>
+                <li className="pb-3">{t("go_to_console")}</li>
+                <li className="pb-3">
+                  {t("have_an_active_phone_number")}{" "}
+                  <a
+                    href="https://support.twilio.com/hc/en-us/articles/223135367-Phone-Number-types-Twilio-offers-and-how-they-work"
+                    className="text-[#2589BD] hover:underline flex items-center"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    {t("read_more")}
+                    <i className="ri-external-link-line ml-1"></i>
+                  </a>
+                </li>
+              </ul>
+              <SmsDemo />
+            </div>
+          )}
         </div>
 
         <ERPModal
@@ -184,14 +178,13 @@ const SMSIntegration: React.FC = () => {
           height={600}
           isForm={true}
           closeModal={() => {
-            setProvider({isOpen: false, information: undefined});
+            setProvider({ isOpen: false, information: undefined });
           }}
-          content={<SMSTwilioConnectPopup data={provider.information}/>}
+          content={<SMSTwilioConnectPopup data={provider.information} />}
         />
       </div>
     </div>
   );
-  // return <></>
 };
 
 export default SMSIntegration;

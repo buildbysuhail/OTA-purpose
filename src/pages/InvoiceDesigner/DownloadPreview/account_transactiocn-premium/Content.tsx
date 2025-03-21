a3bf0d1ece821222b208500be9945942147ca40a
nizam-bit <nizamkarippali007@gmail.com>
2025-03-21 16:20:57 +0530

    multi page mange in acc-trans-premium

--- src/pages/InvoiceDesigner/DownloadPreview/account_transactiocn-premium/Content.tsx
+++ src/pages/InvoiceDesigner/DownloadPreview/account_transactiocn-premium/Content.tsx
@@ -1,11 +1,8 @@
-import { View, Text, Image, StyleSheet } from "@react-pdf/renderer";
-import { AccountTransactionProps } from ".";
-import { dateTrimmer, getAmountInWords } from "../../../../utilities/Utils";
-import useCurrentBranch from "../../../../utilities/hooks/use-current-branch";
-import { TemplateState } from "../../Designer/interfaces";
-import { IndianRupee } from 'lucide-react';
-import { AccTransactionData } from "../../../accounts/transactions/acc-transaction-types";
-import VoucherType from "../../../../enums/voucher-types";
+import { View, Text, Image, StyleSheet } from "@react-pdf/renderer"
+import { dateTrimmer, getAmountInWords } from "../../../../utilities/Utils"
+import type { TemplateState } from "../../Designer/interfaces"
+import type { AccTransactionData } from "../../../accounts/transactions/acc-transaction-types"
+import VoucherType from "../../../../enums/voucher-types"
 
 const styles = StyleSheet.create({
   container: {
@@ -22,7 +19,6 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
-    // flexWrap: "wrap",
     width: "100%",
     zIndex: 10,
   },
@@ -36,7 +32,7 @@ const styles = StyleSheet.create({
   orgAddress: {
     display: "flex",
     flexDirection: "column",
-    gap: 2
+    gap: 2,
   },
   docTitle: {
     fontSize: 18,
@@ -47,10 +43,9 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "row",
     gap: 30,
-    flexWrap: "wrap",
     width: "100%",
     zIndex: 10,
-    marginVertical: 40,
+    marginVertical: 20,
   },
   amountReceived: {
     height: 120,
@@ -72,13 +67,11 @@ const styles = StyleSheet.create({
     width: "40%",
     justifyContent: "flex-start",
     alignItems: "flex-end",
-
   },
   notes: {
     display: "flex",
     flexDirection: "column",
-    marginVertical: 20,
-
+    marginVertical: 10,
   },
   payment: {
     display: "flex",
@@ -91,35 +84,46 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    flexBasis: "50%"
+    flexBasis: "50%",
   },
-});
+})
 
-export const Content = ({ data, template, currentBranch, docIDKey, currency }: { data: AccTransactionData; template?: TemplateState; currentBranch: any, docIDKey?: string; currency?: string; }) => {
-  
-  const logoWidthRatio = template?.headerState?.logoSize ? template.headerState?.logoSize / 100 : 0.5;
-  const headerState = template?.headerState;
-  const totalState = template?.totalState;
-  const footerState = template?.footerState;
+export const Content = ({
+  data,
+  template,
+  currentBranch,
+  docIDKey,
+  currency,
+}: {
+  data: AccTransactionData
+  template?: TemplateState
+  currentBranch: any
+  docIDKey?: string
+  currency?: string
+}) => {
+  const logoWidthRatio = template?.headerState?.logoSize ? template.headerState?.logoSize / 100 : 0.5
+  const headerState = template?.headerState
+  const totalState = template?.totalState
+  const footerState = template?.footerState
 
-  const titleColor = template?.headerState?.docTitleFontColor || "#000";
-  const titleFontSize = headerState?.docTitleFontSize || 16;
+  const titleColor = template?.headerState?.docTitleFontColor || "#000"
+  const titleFontSize = headerState?.docTitleFontSize || 16
 
-  const fontFamily = template?.propertiesState?.font_family || "Roboto";
-  const fontSize = template?.propertiesState?.font_size || 12;
-  const color = template?.propertiesState?.font_color || "#000";
-  const fontWeight = template?.propertiesState?.font_weight || 400;
-  const fontStyle = template?.propertiesState?.fontStyle || "normal";
+  const fontFamily = template?.propertiesState?.font_family || "Roboto"
+  const fontSize = template?.propertiesState?.font_size || 12
+  const color = template?.propertiesState?.font_color || "#000"
+  const fontWeight = template?.propertiesState?.font_weight || 400
+  const fontStyle = template?.propertiesState?.fontStyle || "normal"
 
-  const labelFontSize = template?.propertiesState?.label_font_size || 12;
-  const labelColor = template?.propertiesState?.label_font_color || "#000";
-  const labelFontWeight = template?.propertiesState?.label_font_weight || 400;
-  const labelFontStyle = template?.propertiesState?.label_font_style || "normal";
+  const labelFontSize = template?.propertiesState?.label_font_size || 12
+  const labelColor = template?.propertiesState?.label_font_color || "#000"
+  const labelFontWeight = template?.propertiesState?.label_font_weight || 400
+  const labelFontStyle = template?.propertiesState?.label_font_style || "normal"
 
-  const custNameFontColor = headerState?.customerNameFontColor;
-  const custNameFontSize = headerState?.customerNameFontSize || 12;
+  const custNameFontColor = headerState?.customerNameFontColor
+  const custNameFontSize = headerState?.customerNameFontSize || 12
 
-  const docTitleVal = headerState?.docTitle;
+  const docTitleVal = headerState?.docTitle
 
   const labelStyles = {
     color: labelColor,
@@ -127,7 +131,7 @@ export const Content = ({ data, template, currentBranch, docIDKey, currency }: {
     fontWeight: labelFontWeight,
     fontStyle: labelFontStyle,
     fontFamily,
-  };
+  }
 
   const fontStyles = {
     color,
@@ -135,30 +139,36 @@ export const Content = ({ data, template, currentBranch, docIDKey, currency }: {
     fontWeight,
     fontStyle,
     fontFamily,
-  };
+  }
   const isValidSignature = (logo: any): boolean => {
-    if (!logo) return false;
-    if (typeof logo !== 'string') return false;
-    if (logo.trim() === '') return false;
-    // Add any other specific validation you need
-    return true;
-  };
+    if (!logo) return false
+    if (typeof logo !== "string") return false
+    if (logo.trim() === "") return false
+    return true
+  }
   return (
     <View style={styles.container}>
-
       {/* Doc Title */}
       {headerState?.showDocTitle && (
-        <Text style={[styles.docTitle, {
-          color: titleColor, fontSize: titleFontSize, fontFamily: fontFamily,
-          textAlign: "center", textDecoration: "underline"
-        }]}>
+        <Text
+          style={[
+            styles.docTitle,
+            {
+              color: titleColor,
+              fontSize: titleFontSize,
+              fontFamily: fontFamily,
+              textAlign: "center",
+              textDecoration: "underline",
+            },
+          ]}
+        >
           {docTitleVal}
         </Text>
       )}
 
       {/* Amount Info */}
-      <View style={[styles.amountInfo,]}>
-        <View style={{ flex: 1, display: "flex", flexDirection: "column", gap: 10, width: "66.66%" }}>
+      <View style={styles.amountInfo}>
+        <View style={{ flex: 1, display: "flex", flexDirection: "column", gap: 5, width: "66.66%" }}>
           {headerState?.showNumberField && (
             <View style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "100%" }}>
               <Text style={labelStyles}>{headerState?.numberField || "Payment#"}</Text>
@@ -172,7 +182,6 @@ export const Content = ({ data, template, currentBranch, docIDKey, currency }: {
             <View style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "100%" }}>
               <Text style={labelStyles}>{headerState?.accountTransactionInfo?.dateField || "Payment Date"}</Text>
               <Text style={[fontStyles, { borderBottom: "0.5px solid #DFDFDF", width: "66.66%" }]}>
-                {/* {data.master?.date || "12/5/2024"} */}
                 {dateTrimmer(data.master?.transactionDate)}
               </Text>
             </View>
@@ -180,7 +189,9 @@ export const Content = ({ data, template, currentBranch, docIDKey, currency }: {
 
           {headerState?.accountTransactionInfo?.showReferenceField && (
             <View style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "100%" }}>
-              <Text style={labelStyles}>{headerState?.accountTransactionInfo?.referenceField || "Reference Number"}</Text>
+              <Text style={labelStyles}>
+                {headerState?.accountTransactionInfo?.referenceField || "Reference Number"}
+              </Text>
               <Text style={[fontStyles, { borderBottom: "0.5px solid #DFDFDF", width: "66.66%" }]}>
                 {data?.master.referenceNumber}
               </Text>
@@ -191,31 +202,29 @@ export const Content = ({ data, template, currentBranch, docIDKey, currency }: {
             <View style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "100%" }}>
               <Text style={labelStyles}>{headerState?.accountTransactionInfo?.paymentMode || "Payment Mode"}</Text>
               <Text style={[fontStyles, { borderBottom: "0.5px solid #DFDFDF", width: "66.66%" }]}>
-                {(data?.master?.voucherType == VoucherType.CashPayment || data?.master?.voucherType == VoucherType.CashReceipt || data?.master?.voucherType == VoucherType.CashPaymentEstimate || data?.master?.voucherType == VoucherType.CashReceiptEstimate) ? "Cash" :
-                  (data?.master?.voucherType == VoucherType.BankPayment || data?.master?.voucherType == VoucherType.BankReceipt || data?.master?.voucherType == VoucherType.BankReconciliation) ? "Bank" : ""}
-
+                {data?.master?.voucherType == VoucherType.CashPayment ||
+                data?.master?.voucherType == VoucherType.CashReceipt ||
+                data?.master?.voucherType == VoucherType.CashPaymentEstimate ||
+                data?.master?.voucherType == VoucherType.CashReceiptEstimate
+                  ? "Cash"
+                  : data?.master?.voucherType == VoucherType.BankPayment ||
+                      data?.master?.voucherType == VoucherType.BankReceipt ||
+                      data?.master?.voucherType == VoucherType.BankReconciliation
+                    ? "Bank"
+                    : ""}
               </Text>
             </View>
           )}
 
-
           <View style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "100%" }}>
             <Text style={labelStyles}>Sample Tax1(4.70%)</Text>
-            <Text style={[fontStyles, { borderBottom: "0.5px solid #DFDFDF", width: "66.66%" }]}>
-              $11.75
-
-            </Text>
+            <Text style={[fontStyles, { borderBottom: "0.5px solid #DFDFDF", width: "66.66%" }]}>$11.75</Text>
           </View>
-
 
           <View style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "100%" }}>
             <Text style={labelStyles}>Sample Tax2(7.00%)</Text>
-            <Text style={[fontStyles, { borderBottom: "0.5px solid #DFDFDF", width: "66.66%" }]}>
-              $21.75
-
-            </Text>
+            <Text style={[fontStyles, { borderBottom: "0.5px solid #DFDFDF", width: "66.66%" }]}>$21.75</Text>
           </View>
-
 
           {headerState?.accountTransactionInfo?.showAmountInWords && (
             <View style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "100%" }}>
@@ -225,27 +234,31 @@ export const Content = ({ data, template, currentBranch, docIDKey, currency }: {
               </Text>
             </View>
           )}
-
-
         </View>
 
-        <View style={[styles.amountReceived,
-        {
-          backgroundColor: headerState?.accountTransactionInfo?.amtReceivedBgColor ?? "#65a30d",
-          color: headerState?.accountTransactionInfo?.amtReceivedFontColor ?? "#ffffff",
-          fontWeight: "medium",
-        }]}>
-          <Text style={{ fontSize: 14 }}>{headerState?.accountTransactionInfo?.amtReceivedLabel || "Amount Received"}</Text>
-
-          <Text style={{ fontSize: headerState?.accountTransactionInfo?.amtReceivedFontSize ?? 14 }}>
-            {headerState?.accountTransactionInfo?.currencySymbolPosition === 'before' ? "INR" : ""} {data.master?.totalAmount} {headerState?.accountTransactionInfo?.currencySymbolPosition === 'after' ? "INR" : ""}
+        <View
+          style={[
+            styles.amountReceived,
+            {
+              backgroundColor: headerState?.accountTransactionInfo?.amtReceivedBgColor ?? "#65a30d",
+              color: headerState?.accountTransactionInfo?.amtReceivedFontColor ?? "#ffffff",
+              fontWeight: "medium",
+            },
+          ]}
+        >
+          <Text style={{ fontSize: 14 }}>
+            {headerState?.accountTransactionInfo?.amtReceivedLabel || "Amount Received"}
           </Text>
-
+          <Text style={{ fontSize: headerState?.accountTransactionInfo?.amtReceivedFontSize ?? 14 }}>
+            {headerState?.accountTransactionInfo?.currencySymbolPosition === "before" ? "INR" : ""}{" "}
+            {data.master?.totalAmount}{" "}
+            {headerState?.accountTransactionInfo?.currencySymbolPosition === "after" ? "INR" : ""}
+          </Text>
         </View>
       </View>
 
       {/* Bill To and Signature */}
-      <View style={[styles.companyInfo, { marginVertical: 20 }]}>
+      <View style={[styles.companyInfo, { marginVertical: 10 }]}>
         <View style={{ display: "flex", flexDirection: "column", gap: 1 }}>
           {headerState?.showReceivedFrom && (
             <View>
@@ -255,10 +268,13 @@ export const Content = ({ data, template, currentBranch, docIDKey, currency }: {
           {headerState?.hasBillTo && (
             <View style={styles.billTo}>
               <Text style={labelStyles}>{headerState?.billTo ?? "Bill T0"}</Text>
-              <Text style={{
-                ...fontStyles,
-                color: custNameFontColor, fontSize: custNameFontSize
-              }}>
+              <Text
+                style={{
+                  ...fontStyles,
+                  color: custNameFontColor,
+                  fontSize: custNameFontSize,
+                }}
+              >
                 "Nizam Karippali"
               </Text>
               <Text style={fontStyles}>Dubai</Text>
@@ -277,56 +293,53 @@ export const Content = ({ data, template, currentBranch, docIDKey, currency }: {
         {footerState?.showSignature && (
           <View style={styles.signature}>
             <Text style={fontStyles}>{footerState?.signatureLabel ?? "Authority Signature"}</Text>
-            <View style={{ width: "100%", borderBottom: "0.5px solid #DFDFDF", display: "flex", justifyContent: "flex-end" }}>
-            {headerState?.showLogo && isValidSignature(currentBranch?.logo) && (
-            <Image
-              src={currentBranch.logo}
-              style={{ width: 80 * logoWidthRatio }}
-            />
-          )}
+            <View
+              style={{
+                width: "100%",
+                borderBottom: "0.5px solid #DFDFDF",
+                display: "flex",
+                justifyContent: "flex-end",
+              }}
+            >
+              {headerState?.showLogo && isValidSignature(currentBranch?.logo) && (
+                <Image src={currentBranch.logo || "/placeholder.svg"} style={{ width: 80 * logoWidthRatio }} />
+              )}
             </View>
-
           </View>
         )}
       </View>
 
       {/* Notes */}
       {template?.footerState?.showNotesLabel && (
-        <View style={[styles.notes,]}>
+        <View style={styles.notes}>
           <Text style={labelStyles}>{template?.footerState?.notesLabel || "Notes"}</Text>
           <Text style={{ ...fontStyles, fontSize: template?.footerState?.noteFontSize }}>
             {/* {data?.master?.notes ?? "Payment has been received by cash"} */}
           </Text>
         </View>
       )}
+
       {/* Refund */}
-      <View style={{ width: "100%", borderTop: "0.5px solid #DFDFDF", marginVertical: 10 }} />
-      <View style={styles.payment} >
-
+      <View style={{ width: "100%", borderTop: "0.5px solid #DFDFDF", marginVertical: 5 }} />
+      <View style={styles.payment}>
         <View style={[styles.paymentCol]}>
-          <Text style={labelStyles}>{template?.headerState?.accountTransactionInfo?.paymentRefund || "Total Discount"}</Text>
-          <Text style={fontStyles}>
-            {/* //demo */}
-            {data?.master?.totDiscount ?? "5.00$"}
+          <Text style={labelStyles}>
+            {template?.headerState?.accountTransactionInfo?.paymentRefund || "Total Discount"}
           </Text>
+          <Text style={fontStyles}>{data?.master?.totDiscount ?? "5.00$"}</Text>
         </View>
-
 
         {template?.headerState?.accountTransactionInfo?.showOverPayment && (
           <View style={[styles.paymentCol, { borderLeft: "0.5px solid #DFDFDF" }]}>
-            <Text style={labelStyles}>{template?.headerState?.accountTransactionInfo?.overPayment || "Over Payment"}</Text>
-            <Text style={fontStyles}>
-              {/* //demo */}
-              {data?.master?.totDiscount ?? "5.00$"}
-
+            <Text style={labelStyles}>
+              {template?.headerState?.accountTransactionInfo?.overPayment || "Over Payment"}
             </Text>
+            <Text style={fontStyles}>{data?.master?.totDiscount ?? "5.00$"}</Text>
           </View>
         )}
-
       </View>
-      <View style={{ width: "100%", borderTop: "0.5px solid #DFDFDF", marginVertical: 10 }} />
-
+      <View style={{ width: "100%", borderTop: "0.5px solid #DFDFDF", marginVertical: 5 }} />
     </View>
-  );
-};
+  )
+}
 

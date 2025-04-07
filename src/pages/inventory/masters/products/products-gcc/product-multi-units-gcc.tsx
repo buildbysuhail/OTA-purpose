01180436b668bfc771605616892cb643f01343af
SafvanQrasys <safvan.qrasys@gmail.com>
2025-04-07 20:01:52 +0530

    InvTransactionModified

--- src/pages/inventory/masters/products/products-gcc/product-multi-units-gcc.tsx
+++ src/pages/inventory/masters/products/products-gcc/product-multi-units-gcc.tsx
@@ -24,153 +24,153 @@ const ProductMultiUnitsGCC  : React.FC<{
 
     const renderUnitRows = () => {
         const responseData = getFieldProps("units").value as ProductUnitInputDto[];
-          
-        const units = Array.from({ length: 10 }, (_, i) => i + 1)
-        return units.map((num) => (
-            <tr key={`unit-${num}`} className="h-10">
-                <td className="pr-2">
-                    <div className="flex items-center">
-                        <span className="text-sm font-medium w-12">
-                            {t("unit")} {num}
-                        </span>
-                        <ERPDataCombobox
-                            {...getFieldProps(`multiUnits.unit${num}`)}
-                            noLabel={true}
-                            field={{
-                                getListUrl: Urls.salesRoute,
-                                labelKey: "label",
-                                valueKey: "value",
-                            }}
-                            onChangeData={(data) => handleFieldChange(`multiUnits.unit${num}`, data[`unit${num}`])}
-                            className="w-48"
-                        // placeholder={`Select MultiUnits.Unit${num}`}
-                        />
-                    </div>
-                </td>
-                <td className="px-2">
-                    <ERPInput
-                        id={`unit${num}Qty1`}
-                        noLabel={true}
-                        value={getFieldProps(`quantities.unit${num}Qty1`).value || (num === 1 ? "1" : "0")}
-                        onChange={(e) => handleFieldChange(`quantities.unit${num}Qty1`, e.target.value)}
-                        className={`w-24 text-center ${getFieldProps(`multiUnits.unit${num}`).value ? "bg-[ #fef9c3]" : ""}`}
-                    />
-                </td>
-                <td className="px-2">
-                    {num === 1 ? (
-                        <div className="w-24"></div>
-                    ) : (
-                        <ERPInput
-                            id={`unit${num}Qty2`}
-                            noLabel={true}
-                            value={getFieldProps(`quantities.unit${num}Qty2`).value || "0"}
-                            onChange={(e) => handleFieldChange(`quantities.unit${num}Qty2`, e.target.value)}
-                            className={`w-24 text-center ${getFieldProps(`multiUnits.unit${num}`).value ? "bg-[ #fef9c3]" : ""}`}
-                        />
-                    )}
-                </td>
-                <td className="px-2">
-                    <ERPInput
-                        id={`unit${num}Barcode`}
-                        noLabel={true}
-                        value={getFieldProps(`barcodes.unit${num}Barcode`).value || ""}
-                        onChange={(e) => handleFieldChange(`barcodes.unit${num}Barcode`, e.target.value)}
-                        className="w-32"
-                        disabled={!getFieldProps("config.enableBarcodes").value}
-                    />
-                </td>
-                <td className="px-2">
-                    <ERPInput
-                        id={`unit${num}Price`}
-                        noLabel={true}
-                        value={getFieldProps(`prices.unit${num}Price`).value || "0.00"}
-                        onChange={(e) => handleFieldChange(`prices.unit${num}Price`, e.target.value)}
-                        className="w-32 text-right"
-                    />
-                </td>
-                <td className="pl-2">
-                    <ERPInput
-                        id={`unit${num}Remark`}
-                        noLabel={true}
-                        value={getFieldProps(`remarks.unit${num}Remark`).value || ""}
-                        onChange={(e) => handleFieldChange(`remarks.unit${num}Remark`, e.target.value)}
-                        className="w-40"
-                    />
-                </td>
-            </tr>
-        ))
+       return (<></>)   
+        // const units = Array.from({ length: 10 }, (_, i) => i + 1)
+        // return units.map((num) => (
+        //     <tr key={`unit-${num}`} className="h-10">
+        //         <td className="pr-2">
+        //             <div className="flex items-center">
+        //                 <span className="text-sm font-medium w-12">
+        //                     {t("unit")} {num}
+        //                 </span>
+        //                 <ERPDataCombobox
+        //                     {...getFieldProps(`multiUnits.unit${num}`)}
+        //                     noLabel={true}
+        //                     field={{
+        //                         getListUrl: Urls.salesRoute,
+        //                         labelKey: "label",
+        //                         valueKey: "value",
+        //                     }}
+        //                     onChangeData={(data) => handleFieldChange(`multiUnits.unit${num}`, data[`unit${num}`])}
+        //                     className="w-48"
+        //                 // placeholder={`Select MultiUnits.Unit${num}`}
+        //                 />
+        //             </div>
+        //         </td>
+        //         <td className="px-2">
+        //             <ERPInput
+        //                 id={`unit${num}Qty1`}
+        //                 noLabel={true}
+        //                 value={getFieldProps(`quantities.unit${num}Qty1`).value || (num === 1 ? "1" : "0")}
+        //                 onChange={(e) => handleFieldChange(`quantities.unit${num}Qty1`, e.target.value)}
+        //                 className={`w-24 text-center ${getFieldProps(`multiUnits.unit${num}`).value ? "bg-[ #fef9c3]" : ""}`}
+        //             />
+        //         </td>
+        //         <td className="px-2">
+        //             {num === 1 ? (
+        //                 <div className="w-24"></div>
+        //             ) : (
+        //                 <ERPInput
+        //                     id={`unit${num}Qty2`}
+        //                     noLabel={true}
+        //                     value={getFieldProps(`quantities.unit${num}Qty2`).value || "0"}
+        //                     onChange={(e) => handleFieldChange(`quantities.unit${num}Qty2`, e.target.value)}
+        //                     className={`w-24 text-center ${getFieldProps(`multiUnits.unit${num}`).value ? "bg-[ #fef9c3]" : ""}`}
+        //                 />
+        //             )}
+        //         </td>
+        //         <td className="px-2">
+        //             <ERPInput
+        //                 id={`unit${num}Barcode`}
+        //                 noLabel={true}
+        //                 value={getFieldProps(`barcodes.unit${num}Barcode`).value || ""}
+        //                 onChange={(e) => handleFieldChange(`barcodes.unit${num}Barcode`, e.target.value)}
+        //                 className="w-32"
+        //                 disabled={!getFieldProps("config.enableBarcodes").value}
+        //             />
+        //         </td>
+        //         <td className="px-2">
+        //             <ERPInput
+        //                 id={`unit${num}Price`}
+        //                 noLabel={true}
+        //                 value={getFieldProps(`prices.unit${num}Price`).value || "0.00"}
+        //                 onChange={(e) => handleFieldChange(`prices.unit${num}Price`, e.target.value)}
+        //                 className="w-32 text-right"
+        //             />
+        //         </td>
+        //         <td className="pl-2">
+        //             <ERPInput
+        //                 id={`unit${num}Remark`}
+        //                 noLabel={true}
+        //                 value={getFieldProps(`remarks.unit${num}Remark`).value || ""}
+        //                 onChange={(e) => handleFieldChange(`remarks.unit${num}Remark`, e.target.value)}
+        //                 className="w-40"
+        //             />
+        //         </td>
+        //     </tr>
+        // ))
     }
-
-    return (
-        <div className="flex flex-col gap-4">
-            <div className="w-full">
-                <table className="w-full border-collapse">
-                    <thead>
-                        <tr className="h-10">
-                            <th className="text-left"></th>
-                            <th className="text-left"></th>
-                            <th className="text-left"></th>
-                            <th className="text-left">
-                                <div className="flex items-center">
-                                    <ERPCheckbox
-                                        {...getFieldProps("config.enableBarcodes")}
-                                        label={t("barcode")}
-                                        onChangeData={(data) => handleFieldChange("config.enableBarcodes", data.enableBarcodes)}
-                                    />
-                                </div>
-                            </th>
-                            <th className="text-left">
-                                <span className="text-sm font-medium">{t("price")}</span>
-                            </th>
-                            <th className="text-left">
-                                <span className="text-sm font-medium">{t("remarks")}</span>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>{renderUnitRows()}</tbody>
-                </table>
-            </div>
-            {/* Default Units Section */}
-            <div className="p-4 rounded-md shadow">
-                <h2 className="text-xl font-semibold mb-4">{t("default_units")}</h2>
-                <div className="grid grid-cols-6 gap-4">
-                    <ERPDataCombobox
-                        {...getFieldProps("batch.defSalesUnitID")}
-                        label={t("sales")}
-                        field={{
-                            getListUrl: Urls.salesRoute,
-                            labelKey: "label",
-                            valueKey: "value",
-                        }}
-                        onChangeData={(data) => handleFieldChange("batch.defSalesUnitID", data.defSalesUnitID)}
-                        className="w-full"
-                    />
-                    <ERPDataCombobox
-                        {...getFieldProps("batch.defPurchaseUnitID")}
-                        label={t("purchase")}
-                        field={{
-                            getListUrl: "vajid",
-                            labelKey: "label",
-                            valueKey: "value",
-                        }}
-                        onChangeData={(data) => handleFieldChange("batch.defPurchaseUnitID", data.defPurchaseUnitID)}
-                        className="w-full"
-                    />
-                    <ERPDataCombobox
-                        {...getFieldProps("batch.defReportUnitID")}
-                        label={t("report")}
-                        field={{
-                            getListUrl: "vajid",
-                            labelKey: "label",
-                            valueKey: "value",
-                        }}
-                        onChangeData={(data) => handleFieldChange("batch.defReportUnitID", data.defReportUnitID)}
-                        className="w-full"
-                    />
-                </div>
-            </div>
-        </div>
-    )
+return (<></>)
+    // return (
+    //     <div className="flex flex-col gap-4">
+    //         <div className="w-full">
+    //             <table className="w-full border-collapse">
+    //                 <thead>
+    //                     <tr className="h-10">
+    //                         <th className="text-left"></th>
+    //                         <th className="text-left"></th>
+    //                         <th className="text-left"></th>
+    //                         <th className="text-left">
+    //                             <div className="flex items-center">
+    //                                 <ERPCheckbox
+    //                                     {...getFieldProps("config.enableBarcodes")}
+    //                                     label={t("barcode")}
+    //                                     onChangeData={(data) => handleFieldChange("config.enableBarcodes", data.enableBarcodes)}
+    //                                 />
+    //                             </div>
+    //                         </th>
+    //                         <th className="text-left">
+    //                             <span className="text-sm font-medium">{t("price")}</span>
+    //                         </th>
+    //                         <th className="text-left">
+    //                             <span className="text-sm font-medium">{t("remarks")}</span>
+    //                         </th>
+    //                     </tr>
+    //                 </thead>
+    //                 <tbody>{renderUnitRows()}</tbody>
+    //             </table>
+    //         </div>
+    //         {/* Default Units Section */}
+    //         <div className="p-4 rounded-md shadow">
+    //             <h2 className="text-xl font-semibold mb-4">{t("default_units")}</h2>
+    //             <div className="grid grid-cols-6 gap-4">
+    //                 <ERPDataCombobox
+    //                     {...getFieldProps("batch.defSalesUnitID")}
+    //                     label={t("sales")}
+    //                     field={{
+    //                         getListUrl: Urls.salesRoute,
+    //                         labelKey: "label",
+    //                         valueKey: "value",
+    //                     }}
+    //                     onChangeData={(data) => handleFieldChange("batch.defSalesUnitID", data.defSalesUnitID)}
+    //                     className="w-full"
+    //                 />
+    //                 <ERPDataCombobox
+    //                     {...getFieldProps("batch.defPurchaseUnitID")}
+    //                     label={t("purchase")}
+    //                     field={{
+    //                         getListUrl: "vajid",
+    //                         labelKey: "label",
+    //                         valueKey: "value",
+    //                     }}
+    //                     onChangeData={(data) => handleFieldChange("batch.defPurchaseUnitID", data.defPurchaseUnitID)}
+    //                     className="w-full"
+    //                 />
+    //                 <ERPDataCombobox
+    //                     {...getFieldProps("batch.defReportUnitID")}
+    //                     label={t("report")}
+    //                     field={{
+    //                         getListUrl: "vajid",
+    //                         labelKey: "label",
+    //                         valueKey: "value",
+    //                     }}
+    //                     onChangeData={(data) => handleFieldChange("batch.defReportUnitID", data.defReportUnitID)}
+    //                     className="w-full"
+    //                 />
+    //             </div>
+    //         </div>
+    //     </div>
+    // )
 })
 
 export default ProductMultiUnitsGCC

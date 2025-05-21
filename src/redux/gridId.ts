d77fa0c10f2f48ede4986ff1170ad490a8fb5289
mahirpolosys <mahirpolosys@gmail.com>
2025-05-21 12:19:08 +0530

    update Report  *

--- src/redux/gridId.ts
+++ src/redux/gridId.ts
@@ -10,6 +10,13 @@ const GridId = {
   Purchase_Order_Transit_Report: "Purchase_Order_Transit_Report",
   Purchase_tax: "Purchase_tax",
   itemwise_purchase_summary: "itemwise_purchase_summary",
+  customer_visit_total_visit: "customer_visit_total_visit",
+  customer_visit_last_visit: "customer_visit_last_visit",
+  foc_register_report: "foc_register_report",
+  discount_report_inventory: "discount_report_inventory",
+  discount_report_collection: "discount_report_collection",
+  item_used_for_service: "item_used_for_service",
+  lpo_report: "lpo_report",
 
 };
 

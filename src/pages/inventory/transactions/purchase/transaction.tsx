f6f0282bee0033555b12ccfa3f41eb02ad3b5be2
mahirpolosys <mahirpolosys@gmail.com>
2025-07-04 18:08:44 +0530

    refactor(purchase-transaction): Remove unnecessary loading state
    - Removed redundant loading indicator.
    - Simplified component rendering logic.
    - Improved code readability.

--- src/pages/inventory/transactions/purchase/transaction.tsx
+++ src/pages/inventory/transactions/purchase/transaction.tsx
@@ -1830,7 +1830,6 @@ const TransactionForm: React.FC<TransactionProps> = ({
 
   return (
     <>
-    { !formState.transactionLoading ?(
         <div className="relative">
       {/* <h1>SAFVAN{transactionType}</h1> */}
       {!deviceInfo?.isMobile && (
@@ -2376,9 +2375,6 @@ const TransactionForm: React.FC<TransactionProps> = ({
           t={t} productId={null} rowIndex={formState.serialNoEntryData.rowIndex} />
       )}
     </div>
-    ) : (
-      <>Loading ............</>
-    )}
     </>
   
   );

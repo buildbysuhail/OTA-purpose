59b53172b40b4c0ecc1714cb87fb60ca5368c7e7
mahirpolosys <mahirpolosys@gmail.com>
2025-07-05 15:56:58 +0530

    refactor(purchase): rename prop transactionLoading to fetching

--- src/pages/inventory/transactions/purchase/components/reference-number.tsx
+++ src/pages/inventory/transactions/purchase/components/reference-number.tsx
@@ -33,7 +33,7 @@ const ReferenceNumber = React.forwardRef<
                 label={t(formState.formElements.referenceNumber.label)}
                 value={formState.transaction.master.purchaseInvoiceNumber}
                 className="w-full min-w-[135px]"
-                transactionLoading={formState.transactionLoading}
+                fetching={formState.transactionLoading}
               // transactionLoading={true}
                 onChange={(e) =>
                   dispatch(

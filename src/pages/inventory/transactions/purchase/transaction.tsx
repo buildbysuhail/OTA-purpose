05a815a1657298c76f3be60df7fe2e91dce76c4f
SREERAM <sreeram.polosys@gmail.com>
2025-07-22 15:56:00 +0530

    grid fix

--- src/pages/inventory/transactions/purchase/transaction.tsx
+++ src/pages/inventory/transactions/purchase/transaction.tsx
@@ -1152,7 +1152,7 @@ const TransactionForm: React.FC<TransactionProps> = ({
         caption: t("unit_id"),
         detailsOptionKey: "units",
         allowEditing: true,
-        visible: true,
+        visible: false,
         width: 100,
         alignment: "right",
       },

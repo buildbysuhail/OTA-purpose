16262db0a516926f8f43d3cdc24a1418fa61999f
mahirpolosys <mahirpolosys@gmail.com>
2025-07-24 12:43:55 +0530

    feat(purchase): add apply discounts button
    - Added button to apply discounts to items
    - Updated BillDiscountInput component
    - Updated TransactionFooter component
    - Added applyDiscountsToItems prop
    - Implemented discount application logic

--- src/pages/inventory/transactions/purchase/components/BillDiscountInput.tsx
+++ src/pages/inventory/transactions/purchase/components/BillDiscountInput.tsx
@@ -3,11 +3,13 @@ import ERPInput from "../../../../../components/ERPComponents/erp-input";
 import { VoucherElementProps } from "../../purchase/transaction-types";
 import { formStateMasterHandleFieldChange } from "../reducer";
 import { useDebouncedInput } from "../../../../../utilities/hooks/useDebounce";
+import { ChevronUp } from "lucide-react";
 
 interface BillDiscountInputProps extends VoucherElementProps {
   handleKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>, field: string) => void;
   dispatch: any;
   footerLayout?: 'horizontal' | 'vertical';
+  applyDiscountsToItems?: () => void; // Add this line
 }
 
 const BillDiscountInput: React.FC<BillDiscountInputProps> = ({
@@ -16,6 +18,7 @@ const BillDiscountInput: React.FC<BillDiscountInputProps> = ({
   t,
   handleKeyDown,
   footerLayout,
+  applyDiscountsToItems, // Add this line
 }) => {
   const { value, onChange } = useDebouncedInput(
     formState.transaction.master.billDiscount || '',
@@ -47,6 +50,28 @@ const BillDiscountInput: React.FC<BillDiscountInputProps> = ({
       disabled={
         formState.formElements.billDiscount?.disabled ||
         formState.formElements.pnlMasters?.disabled
+      }
+      prefix={
+        <button
+          type="button"
+          style={{
+            background: "#d32828",
+            border: "none",
+            borderRadius: "6px 0 0 6px",
+            padding: "0px 0px",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            cursor: "pointer",
+            marginRight: "-7px",
+            marginLeft: "-7px"
+          }}
+          onClick={() => {
+            if (applyDiscountsToItems) applyDiscountsToItems(); 
+          }}
+        >
+          <ChevronUp size={20} color="#fff" />
+        </button>
       }
     />
   );

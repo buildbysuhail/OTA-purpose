de6f20368af32a2423efb3acad0f48c8937cf15e
mahirpolosys <mahirpolosys@gmail.com>
2025-08-19 12:27:16 +0530

    style(Company-Profile-manage): adjust margin-bottom

--- src/pages/settings/Administration/Company-Profile-manage.tsx
+++ src/pages/settings/Administration/Company-Profile-manage.tsx
@@ -48,7 +48,7 @@ const CompanyProfileManage: React.FC = React.memo(() => {
 
   return (
     <div className=" w-full modal-content">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mb-2">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mb-0">
         <ERPInput
           {...getFieldProps("registeredName")}
           label={t("registered_name")}

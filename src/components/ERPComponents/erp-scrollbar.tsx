9d163fa6fefbfd67a18c0189e8ecf49b94adf9a3
AsharQrasys <ashar.qrasys@gmail.com>
2025-12-26 09:56:47 +0530

    ScrollBar Color change after save fix

--- src/components/ERPComponents/erp-scrollbar.tsx
+++ src/components/ERPComponents/erp-scrollbar.tsx
@@ -4,16 +4,18 @@ import { RootState } from "../../redux/store";
 
 interface CustomScrollbarProps extends React.HTMLAttributes<HTMLDivElement> {
   className?: string;
+  scrollbarColor?: string;
   thumbHeight?: number;
   maxHeight?: string | number;
 }
 
 export const ERPScrollArea = forwardRef<HTMLDivElement, CustomScrollbarProps>(
-  ({ className = '', children, thumbHeight = 20, maxHeight, ...props }, ref) => {
+  ({scrollbarColor = "219,223,225", className = '', children, thumbHeight = 20, maxHeight, ...props }, ref) => {
     const appState = useAppSelector(
       (state: RootState) => state.AppState?.appState
     );
     const scrollbarWidth = appState.scrollbarWidth === 'md' ? 'scrollbar' : 'scrollbar-thin';
+    const formState = useAppSelector((state: RootState) => state.InventoryTransaction);
 
     return (
       <div
@@ -21,9 +23,9 @@ export const ERPScrollArea = forwardRef<HTMLDivElement, CustomScrollbarProps>(
         className={`scrollbar ${scrollbarWidth} ${className} dark:[--scrollbar-thumb:rgb(75,85,99)] dark:[--scrollbar-track:rgb(30,41,59)]`}
         style={{
           maxHeight: maxHeight,
-          "--scrollbar-thumb": `rgb(${appState.scrollbarColor ?? "219,223,225"})`,
+          "--scrollbar-thumb": `rgb(${formState.userConfig?.scrollbarColor ?? "219,223,225"})`,
           "--scrollbar-track": "rgb(241,245,249)",
-          "--tw-scrollbar-thumb": `rgb(${appState.scrollbarColor ?? "219,223,225"})`,
+          "--tw-scrollbar-thumb": `rgb(${formState.userConfig?.scrollbarColor ?? "219,223,225"})`,
           "--tw-scrollbar-track": "rgb(241,245,249)",
         } as React.CSSProperties}
         {...props}

976e56db802d3fed61d1d0f87fab54e076293caa
AsharQrasys <ashar.qrasys@gmail.com>
2026-01-30 17:37:09 +0530

    Ledger details - Add Customer modal in sales(Not Completed), ledgerId to master, and missing columns, presetPriceCategoryId in user config

--- src/pages/inventory/transactions/sales/ledger-details.tsx
+++ src/pages/inventory/transactions/sales/ledger-details.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import ErpDevGrid from "../../../../components/ERPComponents/erp-dev-grid";
 import { DevGridColumn } from "../../../../components/types/dev-grid-column";
 import Urls from "../../../../redux/urls";
@@ -7,6 +7,9 @@ import { RootState } from "../../../../redux/store";
 import ERPCheckbox from "../../../../components/ERPComponents/erp-checkbox";
 import ERPDataCombobox from "../../../../components/ERPComponents/erp-data-combobox";
 import { formStateMasterHandleFieldChange } from "../reducer";
+import ERPModal from "../../../../components/ERPComponents/erp-modal";
+import PartiesManage from "../../../accounts/masters/parties/parties-manage";
+import ERPButton from "../../../../components/ERPComponents/erp-button";
 
 interface LedgerDetailsProps {
   closeModal: () => void;
@@ -18,8 +21,20 @@ const LedgerDetails: React.FC<LedgerDetailsProps> = ({ closeModal, t }) => {
   const [salesRoute, setSalesRoute] = useState(false);
   const [mainSalesRoute, setMainSalesRoute] = useState<any>();
   const [ledgerInitialized, setLedgerInitialized] = useState(false);
+  const [openCustomerAddModal, setOpenCustomerAddModal] = useState(false);  // Customer add master modal opens
+  const MemoizedPartiesManage = useMemo(() => React.memo(PartiesManage), []);
   const dispatch = useDispatch()
   const gridColumns: DevGridColumn[] = [
+    {
+      dataField: "ledgerID",
+      caption: t("ledger_id"),
+      dataType: "string",
+      allowSorting: true,
+      allowSearch: true,
+      allowFiltering: true,
+      width: 150,
+      visible: false,
+    },
     {
       dataField: "partyCode",
       caption: t("party_code"),
@@ -46,6 +61,76 @@ const LedgerDetails: React.FC<LedgerDetailsProps> = ({ closeModal, t }) => {
       allowSearch: true,
       allowFiltering: true,
       width: 250,
+    },
+    {
+      dataField: "address1",
+      caption: t("address1"),
+      dataType: "string",
+      allowSorting: true,
+      allowSearch: true,
+      allowFiltering: true,
+      width: 100,
+      visible: false,
+    },
+    {
+      dataField: "address2",
+      caption: t("address2"),
+      dataType: "string",
+      allowSorting: true,
+      allowSearch: true,
+      allowFiltering: true,
+      width: 100,
+      visible: false,
+    },
+    {
+      dataField: "address3",
+      caption: t("address3"),
+      dataType: "string",
+      allowSorting: true,
+      allowSearch: true,
+      allowFiltering: true,
+      width: 100,
+      visible: false,
+    },
+    {
+      dataField: "mobilePhone",
+      caption: t("mobile_phone"),
+      dataType: "string",
+      allowSorting: true,
+      allowSearch: true,
+      allowFiltering: true,
+      width: 100,
+      visible: false,
+    },
+    {
+      dataField: "routeName",
+      caption: t("route_name"),
+      dataType: "string",
+      allowSorting: true,
+      allowSearch: true,
+      allowFiltering: true,
+      width: 100,
+      visible: false,
+    },
+    {
+      dataField: "taxNumber",
+      caption: t("tax_number"),
+      dataType: "string",
+      allowSorting: true,
+      allowSearch: true,
+      allowFiltering: true,
+      width: 100,
+      visible: false,
+    },
+    {
+      dataField: "partyID",
+      caption: t("party_id"),
+      dataType: "string",
+      allowSorting: true,
+      allowSearch: true,
+      allowFiltering: true,
+      width: 100,
+      visible: false,
     },
   ];
 
@@ -80,7 +165,7 @@ const LedgerDetails: React.FC<LedgerDetailsProps> = ({ closeModal, t }) => {
       const focusedRowKey = grid.option("focusedRowKey");
       dispatch(
         formStateMasterHandleFieldChange({
-          fields: { ledgerID: focusedRowKey.ledgerID },
+          fields: { ledgerID: focusedRowKey },
         })
       );
       closeModal();
@@ -89,29 +174,36 @@ const LedgerDetails: React.FC<LedgerDetailsProps> = ({ closeModal, t }) => {
 
   return (
     <>
-      <div className="flex items-end gap-2">
-        <ERPCheckbox
-          id="salesRoute"
-          label={t("sales_route")}
-          checked={salesRoute}
-          onChange={() => setSalesRoute(!salesRoute)}
-        />
-        <ERPDataCombobox
-          key={mainSalesRoute}
-          field={{
-            id: "salesRouteID",
-            getListUrl: Urls.MainSalesRoute,
-            valueKey: "id",
-            labelKey: "name",
-          }}
-          noLabel={true}
-          id="mainSalesRoute"
-          data={{ mainSalesRoute }}
-          value={mainSalesRoute}
-          disabled={!salesRoute}
-          onChange={(e) => {
-            setMainSalesRoute(e?.value ?? null);
-          }}
+      <div className="flex items-center justify-between gap-2">
+        <div className="flex gap-2 items-end">
+          <ERPCheckbox
+            id="salesRoute"
+            label={t("sales_route")}
+            checked={salesRoute}
+            onChange={() => setSalesRoute(!salesRoute)}
+          />
+          <ERPDataCombobox
+            key={mainSalesRoute}
+            field={{
+              id: "salesRouteID",
+              getListUrl: Urls.MainSalesRoute,
+              valueKey: "id",
+              labelKey: "name",
+            }}
+            noLabel={true}
+            id="mainSalesRoute"
+            data={{ mainSalesRoute }}
+            value={mainSalesRoute}
+            disabled={!salesRoute}
+            onChange={(e) => {
+              setMainSalesRoute(e?.value ?? null);
+            }}
+          />
+        </div>
+        <ERPButton
+          title={t('+')}
+          onClick={()=> setOpenCustomerAddModal(true)}
+          className="px-2 w-fit"
         />
       </div>
       <div className="mt-4">
@@ -119,6 +211,8 @@ const LedgerDetails: React.FC<LedgerDetailsProps> = ({ closeModal, t }) => {
           columns={gridColumns}
           dataUrl={`${Urls.inv_transaction_base}${formState.transactionType}/LedgerList/${salesRoute && mainSalesRoute ? mainSalesRoute : 0}`}
           gridId="ledgerDetailsGrid"
+          keyExpr="ledgerID"
+          showChooserOnGridHead
           height={450}
           hideGridAddButton={true}
           columnHidingEnabled={true}
@@ -146,6 +240,14 @@ const LedgerDetails: React.FC<LedgerDetailsProps> = ({ closeModal, t }) => {
           }}
         />
       </div>
+      <ERPModal
+        isOpen={openCustomerAddModal}
+        title="Customer"
+        width={950}
+        height={700}
+        closeModal={() => setOpenCustomerAddModal(false)}
+        content={<MemoizedPartiesManage type={"Cust"} />}
+      />
     </>
   );
 };

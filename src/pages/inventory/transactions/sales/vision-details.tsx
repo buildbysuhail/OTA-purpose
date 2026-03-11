35a59af997d1bb2afde114f820c8f2075750fb67
AsharQrasys <ashar.qrasys@gmail.com>
2026-03-11 10:36:59 +0530

    Size Chart btn in SO(PartyID issue exist)

--- src/pages/inventory/transactions/sales/vision-details.tsx
+++ src/pages/inventory/transactions/sales/vision-details.tsx
@@ -4,8 +4,17 @@ import { DevGridColumn } from "../../../../components/types/dev-grid-column";
 import Urls from "../../../../redux/urls";
 import ERPButton from "../../../../components/ERPComponents/erp-button";
 import ERPInput from "../../../../components/ERPComponents/erp-input";
-import DataGrid, { Column, Editing, KeyboardNavigation, Paging, RemoteOperations, Scrolling } from "devextreme-react/cjs/data-grid";
+import DataGrid, {
+  Column,
+  Editing,
+  KeyboardNavigation,
+  Paging,
+  RemoteOperations,
+  Scrolling,
+} from "devextreme-react/cjs/data-grid";
 import ERPAlert from "../../../../components/ERPComponents/erp-sweet-alert";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../../redux/store";
 
 interface visionDetailsProps {
   closeModal: () => void;
@@ -16,41 +25,117 @@ interface visionDetailsProps {
 //------------------------------ HERE THE PARTY ID IS NOT GETTING - SO FIXED VALUE IS USED FOR NOW -----------------------------------------
 // --------------------------------- CHANGE THE PARTY ID AFTER GETTING THE PARTY ID ----------------------------------------------------------
 const api = new APIClient();
-const visionDetails: React.FC<visionDetailsProps> = ({ closeModal, t, formState }) => {
-
-  const partyID = formState.transaction.master.ledgerID;  // Need to change
-
+const visionDetails: React.FC<visionDetailsProps> = ({
+  closeModal,
+  t,
+  formState,
+}) => {
+  const applicationSettings = useSelector(
+    (state: RootState) => state.ApplicationSettings
+  );
+  const partyID = formState.transaction.master.ledgerID; // Need to change
+  const IsOptical =
+    applicationSettings.mainSettings.maintainBusinessType === "Opticals"
+      ? true
+      : false;
   useEffect(() => {
     const loadVisionDetails = async () => {
       try {
-        const response = await api.getAsync(`${Urls.inv_transaction_base}${formState.transactionType}/SelectOpticsDetails/${11044}`); // need to change
+        debugger;
+        if (IsOptical) {
+          const response = await api.getAsync(
+            `${Urls.inv_transaction_base}${
+              formState.transactionType
+            }/SelectOpticsDetails/${11044}`
+          ); // need to change
 
-        if (!response || response.length === 0) {
-          setVisionData(defaultVisionRows);
-          return;
-        }
-        setRemarks(response[0].remarks ?? "");
-        const mappedApiRows = response.map((row: any) => ({
-          id: row.id,
-          CType: row.cType?.trim(),
-          RE_SPH: row.rE_SPH,
-          RE_CYL: row.rE_CYL,
-          RE_AXIS: row.rE_AXIS,
-          RE_VN: row.rE_Vn,
-          LE_SPH: row.lE_SPH,
-          LE_CYL: row.lE_CYL,
-          LE_AXIS: row.lE_AXIS,
-          LE_VN: row.lE_Vn,
-        }));
+          if (!response || response.length === 0) {
+            setVisionData(defaultVisionRows);
+            return;
+          }
+          setRemarks(response[0].remarks ?? "");
+          const mappedApiRows = response.map((row: any) => ({
+            id: row.id,
+            CType: row.cType?.trim(),
+            RE_SPH: row.rE_SPH,
+            RE_CYL: row.rE_CYL,
+            RE_AXIS: row.rE_AXIS,
+            RE_VN: row.rE_Vn,
+            LE_SPH: row.lE_SPH,
+            LE_CYL: row.lE_CYL,
+            LE_AXIS: row.lE_AXIS,
+            LE_VN: row.lE_Vn,
+          }));
 
-        const mergedRows = defaultVisionRows.map((defaultRow) => {
-          const apiRow = mappedApiRows.find(
-            (r: any) => r.CType === defaultRow.CType
+          const mergedRows = defaultVisionRows.map((defaultRow) => {
+            const apiRow = mappedApiRows.find(
+              (r: any) => r.CType === defaultRow.CType
+            );
+            return apiRow ? apiRow : defaultRow;
+          });
+
+          setVisionData(mergedRows);
+        } else {
+          const measurements = await api.getAsync(
+            `${Urls.inv_transaction_base}${formState.transactionType}/Data/MeasurementChart`
           );
-          return apiRow ? apiRow : defaultRow;
-        });
 
-        setVisionData(mergedRows);
+          if (!measurements || measurements.length === 0) {
+            ERPAlert.show({
+              title: t("measurements_not_found"),
+              text: t("please_add_measurements"),
+              icon: "info",
+              showCancelButton: false,
+            });
+            closeModal();
+            return;
+          } else {
+            const response = await api.getAsync(
+              `${Urls.inv_transaction_base}${
+                formState.transactionType
+              }/SelectOpticsDetails/${11044}`
+            );
+            if (!response || response.length === 0) {
+              const measurementRows = measurements.map((row: any) => ({
+                id: 0,
+                CType: row.name,
+                RE_SPH: "",
+                RE_CYL: "",
+                RE_AXIS: "",
+                RE_VN: "",
+                LE_SPH: "",
+                LE_CYL: "",
+                LE_AXIS: "",
+                LE_VN: "",
+              }));
+
+              setVisionData(measurementRows);
+              return;
+            } else {
+              setRemarks(response[0].remarks ?? "");
+              const mappedApiRows = response.map((row: any) => ({
+                id: row.id,
+                CType: row.cType?.trim(),
+                RE_SPH: row.rE_SPH,
+                RE_CYL: row.rE_CYL,
+                RE_AXIS: row.rE_AXIS,
+                RE_VN: "",
+                LE_SPH: "",
+                LE_CYL: "",
+                LE_AXIS: "",
+                LE_VN: "",
+              }));
+
+              const mergedRows = defaultVisionRows.map((defaultRow, index) => {
+                return mappedApiRows[index]
+                  ? { ...defaultRow, ...mappedApiRows[index] }
+                  : defaultRow;
+              });
+
+              setVisionData(mergedRows);
+            }
+          }
+        }
       } catch (error) {
         console.error("Error loading vision details:", error);
         setVisionData(defaultVisionRows);
@@ -64,68 +149,59 @@ const visionDetails: React.FC<visionDetailsProps> = ({ closeModal, t, formState 
     {
       dataField: "CType",
       caption: t("..."),
-      dataType: "string",
       width: 100,
     },
     {
       dataField: "RE_SPH",
-      caption: t("sph"),
-      dataType: "string",
-      width: 50,
+      caption: IsOptical ? t("sph") : t("Inches"),
+      width: IsOptical ? 50 : 140,
     },
     {
       dataField: "RE_CYL",
-      caption: t("cyl"),
-      dataType: "string",
-      width: 50,
+      caption: IsOptical ? t("cyl") : t("CM"),
+      width: IsOptical ? 50 : 140,
     },
     {
       dataField: "RE_AXIS",
-      caption: t("axis"),
-      dataType: "string",
-      width: 50,
+      caption: IsOptical ? t("axis") : t("MM"),
+      width: IsOptical ? 50 : 140,
     },
 
-    {
-      dataField: "RE_VN",
-      caption: t("vn"),
-      dataType: "string",
-      width: 50,
-    },
-
-    {
-      dataField: "LE_SPH",
-      caption: t("sph"),
-      dataType: "string",
-      width: 50,
-    },
-
-    {
-      dataField: "LE_CYL",
-      caption: t("cyl"),
-      dataType: "string",
-      width: 50,
-    },
-
-    {
-      dataField: "LE_AXIS",
-      caption: t("axis"),
-      dataType: "string",
-      width: 50,
-    },
-
-    {
-      dataField: "LE_VN",
-      caption: t("vn"),
-      dataType: "string",
-      width: 50,
-    },
+    ...(IsOptical
+      ? [
+          {
+            dataField: "RE_VN",
+            caption: t("VN"),
+            width: 50,
+          },
+          {
+            dataField: "LE_SPH",
+            caption: t("SPH"),
+            width: 50,
+          },
+          {
+            dataField: "LE_CYL",
+            caption: t("CYL"),
+            width: 50,
+          },
+          {
+            dataField: "LE_AXIS",
+            caption: t("Axis"),
+            width: 50,
+          },
+          {
+            dataField: "LE_VN",
+            caption: t("VN"),
+            width: 50,
+          },
+        ]
+      : []),
   ];
 
   const defaultVisionRows = [
     {
       id: 0,
-      CType: "DV",
+      CType: IsOptical ? "DV" : "",
       RE_SPH: "",
       RE_CYL: "",
       RE_AXIS: "",
@@ -137,7 +213,7 @@ const visionDetails: React.FC<visionDetailsProps> = ({ closeModal, t, formState 
     },
     {
       id: 0,
-      CType: "ADD",
+      CType: IsOptical ? "ADD" : "",
       RE_SPH: "",
       RE_CYL: "",
       RE_AXIS: "",
@@ -149,7 +225,7 @@ const visionDetails: React.FC<visionDetailsProps> = ({ closeModal, t, formState 
     },
     {
       id: 0,
-      CType: "CL",
+      CType: IsOptical ? "CL" : "",
       RE_SPH: "",
       RE_CYL: "",
       RE_AXIS: "",
@@ -189,7 +265,7 @@ const visionDetails: React.FC<visionDetailsProps> = ({ closeModal, t, formState 
       const payload = visionData.map((row: any) => ({
         id: row.id ?? 0,
         branchID: formState.transaction.master.branchID ?? 1,
-        partyID:  11044, // Need to change
+        partyID: 11044, // Need to change
         cType: row.CType,
         rE_SPH: row.RE_SPH,
         rE_CYL: row.RE_CYL,
@@ -199,19 +275,20 @@ const visionDetails: React.FC<visionDetailsProps> = ({ closeModal, t, formState 
         lE_CYL: row.LE_CYL,
         lE_AXIS: row.LE_AXIS,
         lE_Vn: row.LE_VN,
-        remarks: remark ?? ""
-        }));
+        remarks: remark ?? "",
+      }));
 
-        const response = await api.postAsync(
+      const response = await api.postAsync(
         `${Urls.inv_transaction_base}${formState.transactionType}/UpsertOpticsdetails`,
         payload
-        );
-      if(response.isOk === true){
-      ERPAlert.show({
+      );
+      if (response.isOk === true) {
+        ERPAlert.show({
           title: t("details_added_successfully"),
           text: t(""),
           icon: "success",
-      });
+          showCancelButton: false,
+        });
       }
     } catch (err) {
       console.log(err);
@@ -223,10 +300,13 @@ const visionDetails: React.FC<visionDetailsProps> = ({ closeModal, t, formState 
       <div className="flex items-end gap-2 justify-center font-bold text-md">
         {formState.transaction.master.partyName}
       </div>
-      <div className="flex items-end justify-around font-semibold text-md text-blue-950">
-        <div>{t("re")}</div>
-        <div>{t("le")}</div>
-      </div>
+      {IsOptical && (
+        <div className="flex items-end justify-around font-semibold text-lg text-blue-800">
+          <div>{t("RE")}</div>
+          <div>{t("LE")}</div>
+        </div>
+      )}
+
       <div className="mt-2">
         <DataGrid
           keyExpr="CType"
